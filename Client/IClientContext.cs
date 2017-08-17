53a1e79a5bc41cf2c574a4043449fd07e156489f
p2pquake-takuya <takuya@p2pquake.net>
2017-08-17 23:41:20 +0900

    Issue #9: Add Client.IClientContext

---
+++ Client/IClientContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.App;
+using Client.Peer;
+
+namespace Client.Client
+{
+    interface IClientContext
+    {
+        IPeerState PeerState { get; set; }
+
+        IPeerConnector PeerConnector { get; set; }
+
+        /// <summary>
+        /// 参加する
+        /// </summary>
+        /// <returns></returns>
+        bool Join();
+
+        /// <summary>
+        /// 参加終了する
+        /// </summary>
+        void Part();
+
+        /// <summary>
+        /// 接続を維持する
+        /// </summary>
+        /// <returns></returns>
+        bool Maintain();
+    }
+}
