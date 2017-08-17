c1e7fd437c97c66eb292509b0a280a566ab9652a
p2pquake-takuya <takuya@p2pquake.net>
2017-08-17 23:35:38 +0900

    Issue #9: Add App.IPeerState, Remove Client.IPeerIdManager

---
+++ App/IPeerState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Client.General;
+
+namespace Client.App
+{
+    interface IPeerState
+    {
+        int PeerId { get; set; }
+
+        TimeSpan TimeOffset { get; set; }
+
+        KeyData Key { get; set; }
+    }
+}
