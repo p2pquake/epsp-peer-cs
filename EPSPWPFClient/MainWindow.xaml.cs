ef576198c5957adaa25581ab6335878c7ee383e0
typewriter <takuyaxp@gmail.com>
2017-09-07 22:23:44 +0900

    Issue #4: Drawing the japan map with observation points!

--- EPSPWPFClient/MainWindow.xaml.cs
+++ EPSPWPFClient/MainWindow.xaml.cs
@@ -47,7 +47,7 @@ namespace EPSPWPFClient
                         () =>
                         {
                             // MessageBox.Show("ControlActualWidth:" + contentControl.ActualWidth + ", CanvasActualWidth:" + historyControl.canvas.ActualWidth + ", HistoryControlActualWidth:" + historyControl.ActualWidth);
-                            new Quake.QuakeDrawer().Draw(historyControl.svgViewer);
+                            new Quake.QuakeDrawer().Draw(historyControl.canvas);
                         }
                         ), DispatcherPriority.Loaded);
                     break;
