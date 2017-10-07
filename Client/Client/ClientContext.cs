bea4f43adccdff656f569dce7eae8efa6a862ed7
p2pquake-takuya <takuya@p2pquake.net>
2017-10-08 02:12:40 +0900

    Issue #9: Add ClientContext (not implemented)

---
+++ Client/Client/ClientContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.App;
+using Client.Peer;
+
+namespace Client.Client
+{
+    class ClientContext : IClientContext
+    {
+        public ClientState ClientState
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IPeerConnector PeerConnector
+        {
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IPeerStateForClient PeerState
+        {
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public event EventHandler StateChanged;
+
+        public bool Join()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Maintain()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Part()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
