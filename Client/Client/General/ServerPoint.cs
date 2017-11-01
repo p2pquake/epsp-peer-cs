74aea47f31255c80a7a5a861a8b0cc0407ef3042
typewriter <takuyaxp@gmail.com>
2017-11-01 20:59:20 +0900

    Fix #2: Add "servers" configuration

---
+++ Client/Client/General/ServerPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Client.General
+{
+    class ServerPoint
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+        public ServerPoint(string host, int port)
+        {
+            Host = host;
+            Port = port;
+        }
+    }
+}
