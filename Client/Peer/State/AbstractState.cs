968eae9c8effb82185e0aaaf2ed036580c041f6b
p2pquake-takuya <takuya@p2pquake.net>
2017-11-14 21:33:51 +0900

    Issue #13: Implement peer communication (acceptor side)

--- Client/Peer/State/AbstractState.cs
+++ Client/Peer/State/AbstractState.cs
@@ -41,6 +41,7 @@ namespace Client.Peer.State
 
         public virtual void RequireInquiryEcho(Manager.Peer peer, CRLFSocket socket, Packet packet)
         {
+            // FIXME: あとで実装する.
             Relay(peer, socket, packet);
         }
 
@@ -49,15 +50,37 @@ namespace Client.Peer.State
             Relay(peer, socket, packet);
         }
 
+        // 614 Server->Client
         public virtual void RequireProtocolVersion(Manager.Peer peer, CRLFSocket socket, Packet packet)
         {
             string[] datas = { Const.PROTOCOL_VERSION, Const.SOFTWARE_NAME, Const.SOFTWARE_VERSION };
             socket.WriteLine("634 1 " + string.Join(":", datas));
         }
 
+        // 612 Server->Client
         public virtual void RequirePeerId(Manager.Peer peer, CRLFSocket socket, Packet packet)
         {
             socket.WriteLine("632 1 " + peer.GetPeerId());
+        }
+
+        // 634 Client->Server
+        public virtual void ReplyProtocolVersion(Manager.Peer peer, CRLFSocket socket, Packet packet)
+        {
+            if (double.Parse(packet.Data[0]) < Const.ALLOW_PROTOCOL_VERSION)
+            {
+                socket.WriteLine("694 1 Protocol_version_incompatible");
+                socket.Close();
+                return;
+            }
+            
+            socket.WriteLine("612 1");
+        }
+
+        // 632 Client->Server
+        public virtual void ReplyPeerId(Manager.Peer peer, CRLFSocket socket, Packet packet)
+        {
+            // FIXME: 多重接続チェックしてない.
+            peer.PeerData.PeerId = int.Parse(packet.Data[0]);
         }
 
         public virtual void RequirePeerEcho(Manager.Peer peer, CRLFSocket socket, Packet packet)
