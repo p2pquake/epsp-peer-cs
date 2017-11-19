d2290b304746a61cc82e1264c7439dcb26b07412
p2pquake-takuya <takuya@p2pquake.net>
2017-11-19 23:47:33 +0900

    Issue #15: Add IVerifyManager interface

---
+++ Client/Peer/Verifier/IVerifyManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Common.Net;
+
+namespace Client.Peer.Verifier
+{
+    struct VerifyResult
+    {
+        bool IsValid;
+        bool IsInvalidSignature;
+        bool IsExpired;
+    }
+
+    interface IVerifyManager
+    {
+        VerifyResult Verify(Packet packet);
+    }
+}
