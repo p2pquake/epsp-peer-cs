796fda5b18d2c01344639eb0ab3cb10f666dd2ce
p2pquake-takuya <takuya@p2pquake.net>
2017-11-21 21:53:25 +0900

    Issue #15: Implementing VerifyManager

--- Client/Peer/Verifier/VerifyManager.cs
+++ Client/Peer/Verifier/VerifyManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Client.Common.General;
@@ -45,6 +46,20 @@ namespace Client.Peer.Verifier
         {
             // FIXME
             throw new NotImplementedException();
+        }
+
+        private byte[] GenerateSignatureData(string data, string expire)
+        {
+            // MD5導出
+            byte[] dataByteArray = Encoding.GetEncoding(932).GetBytes(data);
+            MD5CryptoServiceProvider md5Provider = new MD5CryptoServiceProvider();
+            byte[] dataMd5 = md5Provider.ComputeHash(dataByteArray);
+
+            // EPSP日時
+            byte[] expireByteArray = Encoding.ASCII.GetBytes(expire);
+
+            // 結合
+            return dataMd5.Concat(expireByteArray).ToArray();
         }
     }
 }
