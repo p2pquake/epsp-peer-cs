8026e8df4c7767c6b3ccf55618a8114a17455f96
typewriter <takuyaxp@gmail.com>
2017-12-12 21:52:36 +0900

    Output eventargs

--- CUIClient/Program.cs
+++ CUIClient/Program.cs
@@ -4,15 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Client.App;
+using StatePrinting;
+using StatePrinting.OutputFormatters;
 
 namespace CUIClient
 {
     class Program
     {
         private static IMediatorContext mediatorContext;
+        private static Stateprinter statePrinter = new Stateprinter();
 
         static void Main(string[] args)
         {
+            statePrinter.Configuration.SetOutputFormatter(new JsonStyle(statePrinter.Configuration));
+
             mediatorContext = new MediatorContext();
             mediatorContext.ConnectionsChanged += MediatorContext_ConnectionsChanged;
             mediatorContext.OnAreapeers += MediatorContext_OnAreapeers;
@@ -22,6 +27,9 @@ namespace CUIClient
             mediatorContext.StateChanged += MediatorContext_StateChanged;
             mediatorContext.Completed += MediatorContext_Completed;
 
+            mediatorContext.IsPortOpen = true;
+            mediatorContext.Port = 6999;
+
             mediatorContext.Connect();
 
             Console.ReadLine();
@@ -29,34 +37,43 @@ namespace CUIClient
 
         private static void MediatorContext_Completed(object sender, Client.Client.OperationCompletedEventArgs e)
         {
-            Console.WriteLine("MediatorContext completed(" + e.Result.ToString() + ", " + e.ErrorCode.ToString() + ")");
-            // throw new NotImplementedException();
+            Console.WriteLine("MediatorContext completed");
+            Console.WriteLine(statePrinter.PrintObject(e));
         }
 
         private static void MediatorContext_StateChanged(object sender, EventArgs e)
         {
             Console.WriteLine("MediatorContext changed to " + mediatorContext.State.ToString());
+            Console.WriteLine(statePrinter.PrintObject(e));
             // throw new NotImplementedException();
         }
 
         private static void MediatorContext_OnUserquake(object sender, Client.Peer.EPSPUserquakeEventArgs e)
         {
-            throw new NotImplementedException();
+            // FIXME: 実装する
+            Console.WriteLine("OnUserquake");
+            Console.WriteLine(statePrinter.PrintObject(e));
         }
 
         private static void MediatorContext_OnTsunami(object sender, Client.Peer.EPSPTsunamiEventArgs e)
         {
-            throw new NotImplementedException();
+            // FIXME: 実装する
+            Console.WriteLine("OnTsunami");
+            Console.WriteLine(statePrinter.PrintObject(e));
         }
 
         private static void MediatorContext_OnEarthquake(object sender, Client.Peer.EPSPQuakeEventArgs e)
         {
-            throw new NotImplementedException();
+            // FIXME: 実装する
+            Console.WriteLine("OnEarthquake");
+            Console.WriteLine(statePrinter.PrintObject(e));
         }
 
         private static void MediatorContext_OnAreapeers(object sender, Client.Peer.EPSPAreapeersEventArgs e)
         {
-            // TODO: will implement
+            // FIXME: 実装する
+            Console.WriteLine("OnAreapeers");
+            Console.WriteLine(statePrinter.PrintObject(e));
         }
 
         private static void MediatorContext_ConnectionsChanged(object sender, EventArgs e)
