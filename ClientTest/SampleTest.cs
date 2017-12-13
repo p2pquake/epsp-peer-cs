d2d5037c045fcb2966bfe115ff61f4c2fb076d31
typewriter <takuyaxp@gmail.com>
2017-12-13 21:13:35 +0900

    Add ClientTest project

---
+++ ClientTest/SampleTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClientTest
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        [TestCase]
+        public void SampleTest_001()
+        {
+            Assert.Pass("NUnit works!");
+        }
+    }
+}
