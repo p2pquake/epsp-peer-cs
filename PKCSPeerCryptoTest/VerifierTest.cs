f1074ade42043915934339561cc915fb6ed4c3ec
p2pquake-takuya <takuya@p2pquake.net>
2018-02-27 23:09:43 +0900

    Add the userquake data verification

--- PKCSPeerCryptoTest/VerifierTest.cs
+++ PKCSPeerCryptoTest/VerifierTest.cs
@@ -99,5 +99,200 @@ namespace PKCSPeerCryptoTest
             VerifyResult result = Verifier.VerifyServerData(summary + detail, expire, signature, DateTime.Parse("2018/02/26 01:39:08"));
             Assert.IsTrue(result.isExpired);
         }
+
+        [TestCase]
+        public void Userquake_Correct()
+        {
+            string dataSignature = "0C/bVWbyqNRCbOijje6ihB/Xk+gcaBiBj8oN13kpP8n9CbesxIidbLp1EzcVAa/C";
+            string dataExpire = "2018/02/27 03-29-32";
+            string publicKey = "MEowDQYJKoZIhvcNAQEBBQADOQAwNgIxAOhqFvL7N/G2RlrluixV6efWsAaAA+9albM86Ltv9MJjwLBiJYAXZPI9UJY++COYOwIBEQ==";
+            string keySignature = "SMCL2D6mR+3LWCa3A5a+j5m3+6G1Utt0zcvVMQTiBhI657RG0n008rNAEuAF02YhA12cNhT6Rji9op7dmPNQhRGDvLCeYQPJuA482Q42Ld4iH9CPNUDDxOJMKN/hJC8LSb0cL+Q7RaCml/+J07cI9VG4nHC9f0X2dhQ5790RkKM=";
+            string keyExpire = "2018/02/27 04-17-21";
+            string data = "1638_270328,525";
+
+            VerifyResult result = Verifier.VerifyUserquake(data, dataExpire, dataSignature, publicKey, keySignature, keyExpire, DateTime.Parse("2018/02/27 03:29:32"));
+            Assert.IsFalse(result.isExpired);
+            Assert.IsTrue(result.isValidSignature);
+        }
+
+        [TestCase]
+        public void Userquake_DataIsExpired()
+        {
+            string dataSignature = "0C/bVWbyqNRCbOijje6ihB/Xk+gcaBiBj8oN13kpP8n9CbesxIidbLp1EzcVAa/C";
+            string dataExpire = "2018/02/27 03-29-32";
+            string publicKey = "MEowDQYJKoZIhvcNAQEBBQADOQAwNgIxAOhqFvL7N/G2RlrluixV6efWsAaAA+9albM86Ltv9MJjwLBiJYAXZPI9UJY++COYOwIBEQ==";
+            string keySignature = "SMCL2D6mR+3LWCa3A5a+j5m3+6G1Utt0zcvVMQTiBhI657RG0n008rNAEuAF02YhA12cNhT6Rji9op7dmPNQhRGDvLCeYQPJuA482Q42Ld4iH9CPNUDDxOJMKN/hJC8LSb0cL+Q7RaCml/+J07cI9VG4nHC9f0X2dhQ5790RkKM=";
+            string keyExpire = "2018/02/27 04-17-21";
+            string data = "1638_270328,525";
+
+            VerifyResult result = Verifier.VerifyUserquake(data, dataExpire, dataSignature, publicKey, keySignature, keyExpire, DateTime.Parse("2018/02/27 03:29:33"));
+            Assert.IsTrue(result.isExpired);
+            Assert.IsTrue(result.isValidSignature);
+        }
+
+        [TestCase]
+        public void Userquake_InvalidDataSignature_Edited()
+        {
+            string dataSignature = "1C/bVWbyqNRCbOijje6ihB/Xk+gcaBiBj8oN13kpP8n9CbesxIidbLp1EzcVAa/C";
+            string dataExpire = "2018/02/27 03-29-32";
+            string publicKey = "MEowDQYJKoZIhvcNAQEBBQADOQAwNgIxAOhqFvL7N/G2RlrluixV6efWsAaAA+9albM86Ltv9MJjwLBiJYAXZPI9UJY++COYOwIBEQ==";
+            string keySignature = "SMCL2D6mR+3LWCa3A5a+j5m3+6G1Utt0zcvVMQTiBhI657RG0n008rNAEuAF02YhA12cNhT6Rji9op7dmPNQhRGDvLCeYQPJuA482Q42Ld4iH9CPNUDDxOJMKN/hJC8LSb0cL+Q7RaCml/+J07cI9VG4nHC9f0X2dhQ5790RkKM=";
+            string keyExpire = "2018/02/27 04-17-21";
+            string data = "1638_270328,525";
+
+            VerifyResult result = Verifier.VerifyUserquake(data, dataExpire, dataSignature, publicKey, keySignature, keyExpire, DateTime.Parse("2018/02/27 03:29:32"));
+            Assert.IsFalse(result.isExpired);
+            Assert.IsFalse(result.isValidSignature);
+        }
+
+        [TestCase]
+        public void Userquake_InvalidDataSignature_Another()
+        {
+            string dataSignature = "bgU4UOzGjfRKQZLq4Bzr4cShdBsCDnK/UBd5RbCqJ0I0dU97mFLLaNLAKscgN8iX";
+            string dataExpire = "2018/02/27 03-29-32";
+            string publicKey = "MEowDQYJKoZIhvcNAQEBBQADOQAwNgIxAOhqFvL7N/G2RlrluixV6efWsAaAA+9albM86Ltv9MJjwLBiJYAXZPI9UJY++COYOwIBEQ==";
+            string keySignature = "SMCL2D6mR+3LWCa3A5a+j5m3+6G1Utt0zcvVMQTiBhI657RG0n008rNAEuAF02YhA12cNhT6Rji9op7dmPNQhRGDvLCeYQPJuA482Q42Ld4iH9CPNUDDxOJMKN/hJC8LSb0cL+Q7RaCml/+J07cI9VG4nHC9f0X2dhQ5790RkKM=";
+            string keyExpire = "2018/02/27 04-17-21";
+            string data = "1638_270328,525";
+
+            VerifyResult result = Verifier.VerifyUserquake(data, dataExpire, dataSignature, publicKey, keySignature, keyExpire, DateTime.Parse("2018/02/27 03:29:32"));
+            Assert.IsFalse(result.isExpired);
+            Assert.IsFalse(result.isValidSignature);
+        }
+
+        [TestCase]
+        public void Userquake_KeyIsExpired()
+        {
+            string dataSignature = "0C/bVWbyqNRCbOijje6ihB/Xk+gcaBiBj8oN13kpP8n9CbesxIidbLp1EzcVAa/C";
+            string dataExpire = "2018/02/27 03-29-32";
+            string publicKey = "MEowDQYJKoZIhvcNAQEBBQADOQAwNgIxAOhqFvL7N/G2RlrluixV6efWsAaAA+9albM86Ltv9MJjwLBiJYAXZPI9UJY++COYOwIBEQ==";
+            string keySignature = "SMCL2D6mR+3LWCa3A5a+j5m3+6G1Utt0zcvVMQTiBhI657RG0n008rNAEuAF02YhA12cNhT6Rji9op7dmPNQhRGDvLCeYQPJuA482Q42Ld4iH9CPNUDDxOJMKN/hJC8LSb0cL+Q7RaCml/+J07cI9VG4nHC9f0X2dhQ5790RkKM=";
+            string keyExpire = "2018/02/27 04-17-21";
+            string data = "1638_270328,525";
+
+            VerifyResult result = Verifier.VerifyUserquake(data, dataExpire, dataSignature, publicKey, keySignature, keyExpire, DateTime.Parse("2018/02/27 04:17:22"));
+            Assert.IsTrue(result.isExpired);
+            Assert.IsTrue(result.isValidSignature);
+        }
+
+        [TestCase]
+        public void Userquake_InvalidKeySignature_Edited()
+        {
+            string dataSignature = "0C/bVWbyqNRCbOijje6ihB/Xk+gcaBiBj8oN13kpP8n9CbesxIidbLp1EzcVAa/C";
+            string dataExpire = "2018/02/27 03-29-32";
+            string publicKey = "MEowDQYJKoZIhvcNAQEBBQADOQAwNgIxAOhqFvL7N/G2RlrluixV6efWsAaAA+9albM86Ltv9MJjwLBiJYAXZPI9UJY++COYOwIBEQ==";
+            string keySignature = "SMCL2D6MR+3LWCa3A5a+j5m3+6G1Utt0zcvVMQTiBhI657RG0n008rNAEuAF02YhA12cNhT6Rji9op7dmPNQhRGDvLCeYQPJuA482Q42Ld4iH9CPNUDDxOJMKN/hJC8LSb0cL+Q7RaCml/+J07cI9VG4nHC9f0X2dhQ5790RkKM=";
+            string keyExpire = "2018/02/27 04-17-21";
+            string data = "1638_270328,525";
+
+            VerifyResult result = Verifier.VerifyUserquake(data, dataExpire, dataSignature, publicKey, keySignature, keyExpire, DateTime.Parse("2018/02/27 03:29:32"));
+            Assert.IsFalse(result.isExpired);
+            Assert.IsFalse(result.isValidSignature);
+        }
+
+        [TestCase]
+        public void Userquake_InvalidKeySignature_Another()
+        {
+            string dataSignature = "0C/bVWbyqNRCbOijje6ihB/Xk+gcaBiBj8oN13kpP8n9CbesxIidbLp1EzcVAa/C";
+            string dataExpire = "2018/02/27 03-29-32";
+            string publicKey = "MEowDQYJKoZIhvcNAQEBBQADOQAwNgIxAOhqFvL7N/G2RlrluixV6efWsAaAA+9albM86Ltv9MJjwLBiJYAXZPI9UJY++COYOwIBEQ==";
+            string keySignature = "c834gslXCfQLEv7iNnvkwJgyliQBHEJWGQhRmOOLuGF/YPo47ar4mB4TILm1MnIPNBRGHo2SsNh1GzdxPoILStB4Tbg9r7x3s40YDdZzo/3dW6HAnEtwQeponMSy1zsYr1AP3LKIwRaRxvCuwltOhfCfkde0Bqf2BoCHrC++wvc=";
+            string keyExpire = "2018/02/27 09-32-17";
+            string data = "1638_270328,525";
+
+            VerifyResult result = Verifier.VerifyUserquake(data, dataExpire, dataSignature, publicKey, keySignature, keyExpire, DateTime.Parse("2018/02/27 03:29:32"));
+            Assert.IsFalse(result.isExpired);
+            Assert.IsFalse(result.isValidSignature);
+        }
+
+        [TestCase]
+        public void Userquake_AnotherPublicKey()
+        {
+            string dataSignature = "0C/bVWbyqNRCbOijje6ihB/Xk+gcaBiBj8oN13kpP8n9CbesxIidbLp1EzcVAa/C";
+            string dataExpire = "2018/02/27 03-29-32";
+            string publicKey = "MEwwDQYJKoZIhvcNAQEBBQADOwAwOAIxAIMdh5rapUc4iT43bdU5ZsegZ0FR2K7/ESoB8xLl963CLzW+rspHr+eyiHNDfAMdbwIDAQAB";
+            string keySignature = "Dt2GPh6kq+rP+44lS6UQZtc23XlM/nSwNWOdykACiRQ64j1FSTYYYugzJk5pMIJjFmqc9qAAgStD8mdpYx3p2Xkt8P9YdFA/f0UNkeCfvK/XYQ0MJfN0p4YmaZqKbJCmeLf4R1iL+4Qf+TA6DjtHn+8vEum/A4yWs4aiZLHD7zk=";
+            string keyExpire = "2018/02/27 10-01-28";
+            string data = "1638_270328,525";
+
+            VerifyResult result = Verifier.VerifyUserquake(data, dataExpire, dataSignature, publicKey, keySignature, keyExpire, DateTime.Parse("2018/02/27 03:29:32"));
+            Assert.IsFalse(result.isExpired);
+            Assert.IsFalse(result.isValidSignature);
+        }
+
+        [TestCase]
+        public void Userquake_CorruptedDataSignature()
+        {
+            string dataSignature = "SIGNATURE_CORRUPTED";
+            string dataExpire = "2018/02/27 03-29-32";
+            string publicKey = "MEowDQYJKoZIhvcNAQEBBQADOQAwNgIxAOhqFvL7N/G2RlrluixV6efWsAaAA+9albM86Ltv9MJjwLBiJYAXZPI9UJY++COYOwIBEQ==";
+            string keySignature = "SMCL2D6mR+3LWCa3A5a+j5m3+6G1Utt0zcvVMQTiBhI657RG0n008rNAEuAF02YhA12cNhT6Rji9op7dmPNQhRGDvLCeYQPJuA482Q42Ld4iH9CPNUDDxOJMKN/hJC8LSb0cL+Q7RaCml/+J07cI9VG4nHC9f0X2dhQ5790RkKM=";
+            string keyExpire = "2018/02/27 04-17-21";
+            string data = "1638_270328,525";
+
+            VerifyResult result = Verifier.VerifyUserquake(data, dataExpire, dataSignature, publicKey, keySignature, keyExpire, DateTime.Parse("2018/02/27 03:29:32"));
+            Assert.IsFalse(result.isExpired);
+            Assert.IsFalse(result.isValidSignature);
+        }
+
+        [TestCase]
+        public void Userquake_CorruptedKeySignature()
+        {
+            string dataSignature = "0C/bVWbyqNRCbOijje6ihB/Xk+gcaBiBj8oN13kpP8n9CbesxIidbLp1EzcVAa/C";
+            string dataExpire = "2018/02/27 03-29-32";
+            string publicKey = "MEowDQYJKoZIhvcNAQEBBQADOQAwNgIxAOhqFvL7N/G2RlrluixV6efWsAaAA+9albM86Ltv9MJjwLBiJYAXZPI9UJY++COYOwIBEQ==";
+            string keySignature = "SIGNATURE_CORRUPTED";
+            string keyExpire = "2018/02/27 04-17-21";
+            string data = "1638_270328,525";
+
+            VerifyResult result = Verifier.VerifyUserquake(data, dataExpire, dataSignature, publicKey, keySignature, keyExpire, DateTime.Parse("2018/02/27 03:29:32"));
+            Assert.IsFalse(result.isExpired);
+            Assert.IsFalse(result.isValidSignature);
+        }
+
+        [TestCase]
+        public void Userquake_CorruptedPublicKey()
+        {
+            string dataSignature = "0C/bVWbyqNRCbOijje6ihB/Xk+gcaBiBj8oN13kpP8n9CbesxIidbLp1EzcVAa/C";
+            string dataExpire = "2018/02/27 03-29-32";
+            string publicKey = "KEY_CORRUPTED";
+            string keySignature = "SMCL2D6mR+3LWCa3A5a+j5m3+6G1Utt0zcvVMQTiBhI657RG0n008rNAEuAF02YhA12cNhT6Rji9op7dmPNQhRGDvLCeYQPJuA482Q42Ld4iH9CPNUDDxOJMKN/hJC8LSb0cL+Q7RaCml/+J07cI9VG4nHC9f0X2dhQ5790RkKM=";
+            string keyExpire = "2018/02/27 04-17-21";
+            string data = "1638_270328,525";
+
+            VerifyResult result = Verifier.VerifyUserquake(data, dataExpire, dataSignature, publicKey, keySignature, keyExpire, DateTime.Parse("2018/02/27 03:29:32"));
+            Assert.IsFalse(result.isExpired);
+            Assert.IsFalse(result.isValidSignature);
+        }
+
+        [TestCase]
+        public void Userquake_CorruptedDataExpire()
+        {
+            string dataSignature = "0C/bVWbyqNRCbOijje6ihB/Xk+gcaBiBj8oN13kpP8n9CbesxIidbLp1EzcVAa/C";
+            string dataExpire = "EXPIRE_CORRUPTED";
+            string publicKey = "MEowDQYJKoZIhvcNAQEBBQADOQAwNgIxAOhqFvL7N/G2RlrluixV6efWsAaAA+9albM86Ltv9MJjwLBiJYAXZPI9UJY++COYOwIBEQ==";
+            string keySignature = "SMCL2D6mR+3LWCa3A5a+j5m3+6G1Utt0zcvVMQTiBhI657RG0n008rNAEuAF02YhA12cNhT6Rji9op7dmPNQhRGDvLCeYQPJuA482Q42Ld4iH9CPNUDDxOJMKN/hJC8LSb0cL+Q7RaCml/+J07cI9VG4nHC9f0X2dhQ5790RkKM=";
+            string keyExpire = "2018/02/27 04-17-21";
+            string data = "1638_270328,525";
+
+            VerifyResult result = Verifier.VerifyUserquake(data, dataExpire, dataSignature, publicKey, keySignature, keyExpire, DateTime.Parse("2018/02/27 03:29:32"));
+            Assert.IsTrue(result.isExpired);
+            Assert.IsFalse(result.isValidSignature);
+        }
+
+        [TestCase]
+        public void Userquake_CorruptedKeyExpire()
+        {
+            string dataSignature = "0C/bVWbyqNRCbOijje6ihB/Xk+gcaBiBj8oN13kpP8n9CbesxIidbLp1EzcVAa/C";
+            string dataExpire = "2018/02/27 03-29-32";
+            string publicKey = "MEowDQYJKoZIhvcNAQEBBQADOQAwNgIxAOhqFvL7N/G2RlrluixV6efWsAaAA+9albM86Ltv9MJjwLBiJYAXZPI9UJY++COYOwIBEQ==";
+            string keySignature = "SMCL2D6mR+3LWCa3A5a+j5m3+6G1Utt0zcvVMQTiBhI657RG0n008rNAEuAF02YhA12cNhT6Rji9op7dmPNQhRGDvLCeYQPJuA482Q42Ld4iH9CPNUDDxOJMKN/hJC8LSb0cL+Q7RaCml/+J07cI9VG4nHC9f0X2dhQ5790RkKM=";
+            string keyExpire = "EXPIRE_CORRUPTED";
+            string data = "1638_270328,525";
+
+            VerifyResult result = Verifier.VerifyUserquake(data, dataExpire, dataSignature, publicKey, keySignature, keyExpire, DateTime.Parse("2018/02/27 03:29:32"));
+            Assert.IsTrue(result.isExpired);
+            Assert.IsFalse(result.isValidSignature);
+        }
     }
 }
