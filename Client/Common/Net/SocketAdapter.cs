d7bd05c85d3dcd195d9e859811708176866562e6
p2pquake-takuya <takuya@p2pquake.net>
2018-03-15 22:28:31 +0900

    Allow mocking the socket

--- Client/Common/Net/SocketAdapter.cs
+++ Client/Common/Net/SocketAdapter.cs
@@ -17,35 +17,35 @@ namespace Client.Common.Net
             this.socket = socket;
         }
 
-        public bool Connected { get { return socket.Connected; } }
-        public EndPoint RemoteEndPoint { get { return socket.RemoteEndPoint; } }
+        public virtual bool Connected { get { return socket.Connected; } }
+        public virtual EndPoint RemoteEndPoint { get { return socket.RemoteEndPoint; } }
 
-        public IAsyncResult BeginConnect(string host, int port, AsyncCallback asyncCallback, ISocket iSocket)
+        public virtual IAsyncResult BeginConnect(string host, int port, AsyncCallback asyncCallback, ISocket iSocket)
         {
             return socket.BeginConnect(host, port, asyncCallback, socket);
         }
 
-        public void BeginReceive(byte[] buffer, int offset, int size, SocketFlags socketFlags, AsyncCallback asyncCallback, ISocket iSocket)
+        public virtual void BeginReceive(byte[] buffer, int offset, int size, SocketFlags socketFlags, AsyncCallback asyncCallback, ISocket iSocket)
         {
             socket.BeginReceive(buffer, offset, size, socketFlags, asyncCallback, socket);
         }
 
-        public void Close()
+        public virtual void Close()
         {
             socket.Close();
         }
 
-        public void EndConnect(IAsyncResult ar)
+        public virtual void EndConnect(IAsyncResult ar)
         {
             socket.EndConnect(ar);
         }
 
-        public void Send(byte[] buffer)
+        public virtual void Send(byte[] buffer)
         {
             socket.Send(buffer);
         }
 
-        public void Shutdown(SocketShutdown both)
+        public virtual void Shutdown(SocketShutdown both)
         {
             socket.Shutdown(both);
         }
