0bcdd3aa489c3c2d0e53f8a85fb59a868aada0e7
typewriter <takuyaxp@gmail.com>
2018-04-03 22:57:52 +0900

    Add Peer#socket_ReadLine code range tests

---
+++ ClientTest/Peer/Manager/PeerReadLineTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Common.Net;
+using NUnit.Framework;
+
+namespace ClientTest.Peer.Manager
+{
+    [TestFixture]
+    class PeerReadLineTest
+    {
+        Client.Peer.Manager.Peer peer;
+
+        [SetUp]
+        public void CreatePeer()
+        {
+            peer = new Client.Peer.Manager.Peer(new Client.Peer.Manager.PeerManager());
+        }
+
+        [TearDown]
+        public void DestroyPeer()
+        {
+            peer = null;
+        }
+
+        private static IEnumerable<TestCaseData> GenerateCodeRange()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                yield return new TestCaseData(i);
+            }
+        }
+
+        [Test, TestCaseSource("GenerateCodeRange")]
+        public void CodeRangeTest(int code)
+        {
+            var packet = new Packet();
+            packet.Code = code;
+            packet.Hop = code;
+            Invoke(packet);
+        }
+
+        private void Invoke(Packet packet)
+        {
+            var eventArgs = new ReadLineEventArgs();
+            eventArgs.packet = packet;
+
+            var type = peer.GetType();
+            type.InvokeMember(
+                "socket_ReadLine",
+                BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.InvokeMethod,
+                null,
+                peer,
+                new object[] { this, eventArgs }
+                );
+        }
+    }
+}
