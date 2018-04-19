d699b993bcefab4c0f0e64c2bdad8eb30675e24d
p2pquake-takuya <takuya@p2pquake.net>
2018-04-19 22:19:06 +0900

    Add Userquake summarizer interfaces

---
+++ Client/Misc/UQSummary/IUQManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Misc.UQSummary
+{
+    public class Userquake
+    {
+        DateTime AbstractTime { get; set; }
+        TimeSpan RelativeTime { get; set; }
+        string AreaCode { get; set; }
+    }
+
+    public interface IUQManager
+    {
+        event EventHandler Occurred;
+
+        Func<DateTime> ProtocolTime { set; }
+        Func<IDictionary<string, int>> AreaPeerDictionary { set; }
+
+        bool IsOnGoing { get; }
+        IUQJudge UQJudge { set; }
+
+        void Add(string areaCode);
+        IDictionary<string, int> GetCurrentSummary();
+    }
+}
