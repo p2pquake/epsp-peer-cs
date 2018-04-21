8334a22fe743acaf544b6fd26af293511fbd436b
typewriter <takuyaxp@gmail.com>
2018-04-21 23:39:57 +0900

    Add "IsOnGoing" reset periodically

--- Client/Misc/UQSummary/UQManager.cs
+++ Client/Misc/UQSummary/UQManager.cs
@@ -8,15 +8,28 @@ namespace Client.Misc.UQSummary
 {
     public class UQManager : IUQManager
     {
+        private const int Interval = 30;
+
         public event EventHandler Occurred = (s, e) => { };
 
         public Func<IDictionary<string, int>> AreaPeerDictionary { private get; set; }
         public Func<DateTime> ProtocolTime { private get; set; }
 
-        public bool IsOnGoing { get; private set; }
+        public bool IsOnGoing
+        {
+            get
+            {
+                if (isOnGoing && ProtocolTime().Subtract(userquakeList.Last().AbstractTime).TotalSeconds >= Interval)
+                {
+                    isOnGoing = false;
+                }
+                return isOnGoing;
+            }
+        }
         public IUQJudge UQJudge { private get; set; }
 
         private List<Userquake> userquakeList = new List<Userquake>();
+        private bool isOnGoing = false;
 
         public void Add(string areaCode)
         {
@@ -26,10 +39,10 @@ namespace Client.Misc.UQSummary
             userquake.RelativeTime = new TimeSpan(0);
 
             if (userquakeList.Count > 0 &&
-                userquake.AbstractTime.Subtract(userquakeList.Last().AbstractTime).TotalSeconds >= 30)
+                userquake.AbstractTime.Subtract(userquakeList.Last().AbstractTime).TotalSeconds >= Interval)
             {
                 userquakeList.Clear();
-                IsOnGoing = false;
+                isOnGoing = false;
             }
 
             if (userquakeList.Count > 0)
@@ -41,7 +54,7 @@ namespace Client.Misc.UQSummary
 
             if (!IsOnGoing && UQJudge.Judge(userquakeList))
             {
-                IsOnGoing = true;
+                isOnGoing = true;
                 Occurred(this, EventArgs.Empty);
             }
         }
