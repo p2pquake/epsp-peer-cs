2f5436f876571f8df1ffb77099e2c72d38fac15e
typewriter <takuyaxp@gmail.com>
2018-04-24 20:52:15 +0900

    Fix class accessibility

--- Client/Misc/UQSummary/SimpleUQJudge.cs
+++ Client/Misc/UQSummary/SimpleUQJudge.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Client.Misc.UQSummary
 {
-    class SimpleUQJudge : IUQJudge
+    public class SimpleUQJudge : IUQJudge
     {
         private int thresholdCount;
 
