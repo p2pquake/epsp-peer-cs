3ded7fea4001fa7dd5bbbcd001c7d0123441eee0
typewriter <takuyaxp@gmail.com>
2018-04-29 13:46:11 +0900

    Add EPSPHandler tests

--- CUIClientTest/Handler/EPSPHandlerTest.cs
+++ CUIClientTest/Handler/EPSPHandlerTest.cs
@@ -16,24 +16,12 @@ namespace CUIClientTest.Handler
     {
         private EPSPHandler epspHandler;
         private DateTime dateTime;
-        //private MemoryStream memoryStream;
-        //private StreamWriter streamWriter;
-        //private TextWriter textWriter;
-        //private StreamReader streamReader;
         private StringWriter stringWriter;
 
         [SetUp]
         public void SetUp()
         {
             epspHandler = new EPSPHandler(GetDateTime);
-            //memoryStream = new MemoryStream();
-            //streamWriter = new StreamWriter(memoryStream);
-            //textWriter = TextWriter.Synchronized(streamWriter);
-            //streamWriter.AutoFlush = true;
-            //Console.SetOut(textWriter);
-
-            //streamReader = new StreamReader(memoryStream);
-
             stringWriter = new StringWriter();
             Console.SetOut(stringWriter);
         }
@@ -41,16 +29,160 @@ namespace CUIClientTest.Handler
         [TearDown]
         public void TearDown()
         {
-            //using (var std = new StreamWriter(Console.OpenStandardOutput()))
-            //{
-            //    std.AutoFlush = true;
-            //    Console.SetOut(std);
-            //}
+            stringWriter.Close();
+        }
 
-            //streamReader.Close();
-            //textWriter.Close();
-            //streamWriter.Close();
-            //memoryStream.Close();
+        [TestCase]
+        public void AreaPeersTest()
+        {
+            var messageList = new List<string>();
+
+            var areapeers = new EPSPAreapeersEventArgs();
+            areapeers.IsInvalidSignature = false;
+            areapeers.IsExpired = false;
+            areapeers.AreaPeerDictionary = new Dictionary<string, int>();
+
+            // 無効な署名データ
+            areapeers.IsExpired = true;
+            epspHandler.MediatorContext_OnAreapeers(this, areapeers);
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+
+            // ゼロデータ
+            areapeers.IsExpired = false;
+            epspHandler.MediatorContext_OnAreapeers(this, areapeers);
+            messageList.Add("地域ピア数の情報を受信しました: ピア数 0");
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+
+            // 有効データ
+            areapeers.AreaPeerDictionary = new Dictionary<string, int>() { { "100", 5 }, { "105", 3 } };
+            epspHandler.MediatorContext_OnAreapeers(this, areapeers);
+            messageList.Add("地域ピア数の情報を受信しました: ピア数 8");
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+        }
+
+        [TestCase]
+        public void EarthquakeTest()
+        {
+            EPSPQuakeEventArgs quake;
+            var messageList = new List<string>();
+
+            // 無効な署名データ
+            quake = new EPSPQuakeEventArgs();
+            quake.IsInvalidSignature = false;
+            quake.IsExpired = true;
+            epspHandler.MediatorContext_OnEarthquake(this, quake);
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+
+            // 共通データ
+            quake = new EPSPQuakeEventArgs();
+            quake.IsInvalidSignature = false;
+            quake.IsExpired = false;
+            quake.OccuredTime = "1日2時3分";
+            quake.Scale = "5弱";
+            quake.TsunamiType = DomesticTsunamiType.None;
+            quake.Destination = "東京湾";
+            quake.Magnitude = "5.8";
+            quake.Depth = "10km";
+            quake.PointList = new List<QuakeObservationPoint>()
+            {
+                new QuakeObservationPoint() { Name = "東京都２３区", Scale = "5弱" },
+                new QuakeObservationPoint() { Name = "神奈川県東部", Scale = "4" },
+                new QuakeObservationPoint() { Name = "東京都多摩東部", Scale = "3" },
+            };
+
+            // 震度速報
+            quake.InformationType = QuakeInformationType.ScalePrompt;
+            epspHandler.MediatorContext_OnEarthquake(this, quake);
+            messageList.Add("地震情報を受信しました: 1日2時3分 震度5弱 ");
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+
+            // 震源情報
+            quake.InformationType = QuakeInformationType.Destination;
+            epspHandler.MediatorContext_OnEarthquake(this, quake);
+            messageList.Add("地震情報を受信しました: 1日2時3分 東京湾(10km, M5.8) 津波の心配なし");
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+
+            // 震度・震源情報
+            quake.InformationType = QuakeInformationType.ScaleAndDestination;
+            epspHandler.MediatorContext_OnEarthquake(this, quake);
+            messageList.Add("地震情報を受信しました: 1日2時3分 震度5弱 東京湾(10km, M5.8) 津波の心配なし");
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+
+            // 各地の震度情報
+            quake.InformationType = QuakeInformationType.Detail;
+            epspHandler.MediatorContext_OnEarthquake(this, quake);
+            messageList.Add("地震情報を受信しました: 1日2時3分 震度5弱 東京湾(10km, M5.8) 津波の心配なし");
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+
+            // 遠地地震情報
+            quake.InformationType = QuakeInformationType.Foreign;
+            epspHandler.MediatorContext_OnEarthquake(this, quake);
+            messageList.Add("地震情報を受信しました: 1日2時3分 東京湾(10km, M5.8) 津波の心配なし");
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+        }
+
+        [TestCase]
+        public void EEWTest()
+        {
+            var messageList = new List<string>();
+
+            var eew = new EPSPEEWTestEventArgs();
+            eew.IsInvalidSignature = false;
+            eew.IsExpired = false;
+            eew.IsTest = true;
+            
+            // 試験報
+            epspHandler.MediatorContext_OnEEWTest(this, eew);
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+            
+            // 無効な署名データ
+            eew.IsTest = false;
+            eew.IsInvalidSignature = true;
+            epspHandler.MediatorContext_OnEEWTest(this, eew);
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+
+            // 有効データ
+            eew.IsInvalidSignature = false;
+            epspHandler.MediatorContext_OnEEWTest(this, eew);
+            messageList.Add("緊急地震速報 配信試験（オープンβ）の情報を受信しました。");
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+        }
+
+        [TestCase]
+        public void TsunamiTest()
+        {
+            var messageList = new List<string>();
+            
+            // 無効な署名データ
+            var tsunami = new EPSPTsunamiEventArgs();
+            tsunami.IsExpired = true;
+            tsunami.IsInvalidSignature = false;
+            epspHandler.MediatorContext_OnTsunami(this, tsunami);
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+
+            // キャンセル報
+            tsunami.IsExpired = false;
+            tsunami.IsCancelled = true;
+            epspHandler.MediatorContext_OnTsunami(this, tsunami);
+            messageList.Add("津波予報は解除されました。");
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+
+            // 有効データ
+            tsunami.IsCancelled = false;
+            tsunami.RegionList = new List<TsunamiForecastRegion>();
+            var region = new TsunamiForecastRegion();
+            region.Category = TsunamiCategory.Advisory;
+            region.IsImmediately = false;
+            region.Region = "北海道太平洋沿岸東部";
+            tsunami.RegionList.Add(region);
+            var region2 = new TsunamiForecastRegion();
+            region2.Category = TsunamiCategory.MajorWarning;
+            region2.IsImmediately = false;
+            region2.Region = "北海道太平洋沿岸中部";
+            tsunami.RegionList.Add(region2);
+            epspHandler.MediatorContext_OnTsunami(this, tsunami);
+            messageList.Add("津波予報を受信しました: [津波注意報]北海道太平洋沿岸東部、[大津波警報]北海道太平洋沿岸中部");
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
         }
 
         [TestCase]
@@ -63,6 +195,12 @@ namespace CUIClientTest.Handler
             userquake.IsExpired = false;
             userquake.AreaCode = "010";
 
+            userquake.IsExpired = true;
+            dateTime = DateTime.Parse("2018/01/01 12:00:00");
+            epspHandler.MediatorContext_OnUserquake(this, userquake);
+            Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
+
+            userquake.IsExpired = false;
             dateTime = DateTime.Parse("2018/01/01 12:00:00");
             epspHandler.MediatorContext_OnUserquake(this, userquake);
             Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
@@ -88,7 +226,7 @@ namespace CUIClientTest.Handler
             epspHandler.MediatorContext_OnUserquake(this, userquake);
             Assert.AreEqual(string.Join("\r\n", messageList), GetWriterOutput());
         }
-        
+
 
         private DateTime GetDateTime()
         {
@@ -97,7 +235,7 @@ namespace CUIClientTest.Handler
 
         private string GetWriterOutput()
         {
-            return Regex.Replace(stringWriter.ToString().TrimEnd(), @"^\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}.\d{3} ", "", RegexOptions.Multiline);
+            return Regex.Replace(stringWriter.ToString().TrimEnd('\r', '\n'), @"^\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}.\d{3} ", "", RegexOptions.Multiline);
         }
     }
 }
