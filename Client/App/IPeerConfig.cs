6e3cb9d032337fc4ada7e55bd0fe6291b6e23aa8
p2pquake-takuya <takuya@p2pquake.net>
2018-05-12 12:55:09 +0900

    Add documentation comments into Client.App classes

--- Client/App/IPeerConfig.cs
+++ Client/App/IPeerConfig.cs
@@ -6,15 +6,25 @@ using System.Threading.Tasks;
 
 namespace Client.App
 {
+    /// <summary>
+    /// EPSPピアの設定を行うインタフェースです。
+    /// </summary>
     public interface IPeerConfig
     {
+        /// <summary>
+        /// 地域コード(3桁)
+        /// <para>0埋めのある本来の地域コードは <see cref="FormattedAreaCode"/> を参照してください。</para>
+        /// </summary>
         int AreaCode { get; set; }
+        /// <summary>地域コード(3桁)</summary>
         string FormattedAreaCode { get; }
 
+        /// <summary>ポート開放するかどうか</summary>
         bool IsPortOpen { get; set; }
-
+        /// <summary>ポート番号</summary>
         int Port { get; set; }
 
+        /// <summary>最大接続数</summary>
         int MaxConnections { get; set; }
     }
 }
