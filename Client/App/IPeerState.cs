19d9f9c22e28b7c07c7829e85c0021ce2e8e82f6
typewriter <takuyaxp@gmail.com>
2018-05-12 12:55:09 +0900

    Add documentation comments into Client.App classes

--- Client/App/IPeerState.cs
+++ Client/App/IPeerState.cs
@@ -8,22 +8,26 @@ using Client.Client.General;
 
 namespace Client.App
 {
+    /// <summary>
+    /// ピアの状態を表すインタフェースです。
+    /// </summary>
     public interface IPeerState
     {
+        /// <summary>ピアID</summary>
         int PeerId { get; }
-
+        /// <summary>プロトコル時刻とシステム時刻の差</summary>
         TimeSpan TimeOffset { get; }
-
+        /// <summary>現在の接続数</summary>
         int Connections { get; }
-
+        /// <summary>ポートが開放されているかどうか</summary>
         bool IsPortOpened { get; }
-
+        /// <summary>情報発信用の鍵情報</summary>
         KeyData Key { get; }
-
+        /// <summary>地域ピア数</summary>
         IDictionary<string, int> AreaPeerDictionary { get; }
-
+        /// <summary>総ピア数</summary>
         int PeerCount { get; }
-
+        /// <summary>プロトコル時刻を演算し、返します。</summary>
         DateTime CalcNowProtocolTime();
     }
 
