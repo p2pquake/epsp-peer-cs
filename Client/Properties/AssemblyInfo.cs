451c1439e90af99ad648909aabf8150d70a1c298
typewriter <takuyaxp@gmail.com>
2018-05-16 22:18:53 +0900

    Update assembly info

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("P2P地震情報#(Sharp)")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("EPSPClient")]
+[assembly: AssemblyDescription("C# implementation of EPSP (P2PQuake)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("P2PQuake developer team")]
 [assembly: AssemblyProduct("")]
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.001")]
-[assembly: AssemblyFileVersion("0.1.0.001")]
+[assembly: AssemblyVersion("0.1.*")]
+//[assembly: AssemblyFileVersion("0.1")]
 
 
 // log4netを使用します
