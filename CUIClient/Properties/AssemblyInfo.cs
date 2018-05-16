cceeab6ae19388122fafb1dbab07e5052208f2bd
p2pquake-takuya <takuya@p2pquake.net>
2018-05-16 22:18:53 +0900

    Update assembly info

--- CUIClient/Properties/AssemblyInfo.cs
+++ CUIClient/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("CUIClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("EPSPCUIClient")]
+[assembly: AssemblyDescription("EPSP command line interface")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CUIClient")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("P2PQuake developer team")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("P2PQuake developer team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+//[assembly: AssemblyFileVersion("0.1")]
