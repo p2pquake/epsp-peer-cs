7476e865468224fd6564598cc1ca121c8976213d
typewriter <takuyaxp@gmail.com>
2018-06-04 23:55:53 +0900

    Implement HistoryViewModel and binding

--- EPSPWPFClient/ViewModel/HistoryViewModel.cs
+++ EPSPWPFClient/ViewModel/HistoryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Reactive.Bindings;
+﻿using Client.Peer;
+using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,22 @@ namespace EPSPWPFClient.ViewModel
     public class History
     {
         public string Title { get; set; }
+        public EPSPDataEventArgs DataEventArgs { get; set; }
     }
 
     public class HistoryViewModel
     {
         public ReactiveProperty<List<History>> HistoryList { get; } = new ReactiveProperty<List<History>>(new List<History>());
+        public ReactiveProperty<int> HistoryIndex { get; } = new ReactiveProperty<int>();
+
+        public HistoryViewModel(Mediator.EPSPHandler epspHandler)
+        {
+            epspHandler.EventList.ToReadOnlyReactiveCollection(epspHandler.EventList.ToCollectionChanged());
+
+            HistoryIndex.Subscribe(e =>
+            {
+                Console.WriteLine(HistoryIndex.Value);
+            });
+        }
     }
 }
