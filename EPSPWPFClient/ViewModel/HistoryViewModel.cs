321d80ce74843b009d937d059c3b2fcf6787d9b7
p2pquake-takuya <takuya@p2pquake.net>
2018-06-06 22:27:53 +0900

    Fix reactive bindings in HistoryControl

--- EPSPWPFClient/ViewModel/HistoryViewModel.cs
+++ EPSPWPFClient/ViewModel/HistoryViewModel.cs
@@ -1,32 +1,45 @@
 ﻿using Client.Peer;
+using EPSPWPFClient.Mediator;
 using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Concurrency;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EPSPWPFClient.ViewModel
 {
-    public class History
+    public class EventViewModel
     {
-        public string Title { get; set; }
+        public string Title { get { return DataEventArgs.GetType().Name; } }
         public EPSPDataEventArgs DataEventArgs { get; set; }
     }
 
     public class HistoryViewModel
     {
-        public ReactiveProperty<List<History>> HistoryList { get; } = new ReactiveProperty<List<History>>(new List<History>());
-        public ReactiveProperty<int> HistoryIndex { get; } = new ReactiveProperty<int>();
+        public ReadOnlyReactiveCollection<EventViewModel> EventList { get; private set; }
+        public ReactiveProperty<int> EventIndex { get; } = new ReactiveProperty<int>();
+        private EPSPHandler epspHandler;
 
-        public HistoryViewModel(Mediator.EPSPHandler epspHandler)
+        public HistoryViewModel(EPSPHandler epspHandler)
         {
-            epspHandler.EventList.ToReadOnlyReactiveCollection(epspHandler.EventList.ToCollectionChanged());
+            this.epspHandler = epspHandler;
 
-            HistoryIndex.Subscribe(e =>
+            EventIndex.Subscribe(e =>
             {
-                Console.WriteLine(HistoryIndex.Value);
+                Console.WriteLine(EventIndex.Value);
             });
+        }
+
+        public void SetScheduler(IScheduler scheduler)
+        {
+            EventList = epspHandler.EventList.ToReadOnlyReactiveCollection(
+                epspHandler.EventList.ToCollectionChanged(),
+                e => new EventViewModel() { DataEventArgs = e },
+                scheduler
+                );
+
         }
     }
 }
