4f89dc3f6cc8fc3b746e15ae86cc48ae2d690dfa
typewriter <takuyaxp@gmail.com>
2018-09-15 22:28:28 +0900

    Support tsunami (only character messages)

--- EPSPWPFClient/Tsunami/TsunamiDrawer.cs
+++ EPSPWPFClient/Tsunami/TsunamiDrawer.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace EPSPWPFClient.Tsunami
 {
@@ -40,13 +41,23 @@ namespace EPSPWPFClient.Tsunami
             var offsetX = (canvas.ActualWidth - img.ActualWidth) / 2;
             var offsetY = (canvas.ActualHeight - img.ActualHeight) / 2;
 
+            {
+                var control = new ContentControl()
+                {
+                    Content = new Rectangle() { Width = img.ActualWidth, Height = img.ActualHeight, Fill = new SolidColorBrush(Color.FromArgb(128, 0, 0, 0)) }
+                };
+                Canvas.SetLeft(control, offsetX + 0);
+                Canvas.SetTop(control, offsetY);
+                canvas.Children.Add(control);
+            }
+
             // 解除
             if (EventArgs.IsCancelled)
             {
-                var message = "津波予報は\n解除されました。";
-
+                var cancelledMessage = "津波予報は解除されました。";
+                
                 {
-                    var text = new TextBlock() { Text = message, FontSize = 24, Foreground = Brushes.Black, Background = new SolidColorBrush(Color.FromArgb(128, 0, 0, 0)) };
+                    var text = new TextBlock() { Text = cancelledMessage, FontSize = 18, Foreground = Brushes.Black };
                     var control = new ContentControl() { Content = text };
                     Canvas.SetLeft(control, offsetX + 0);
                     Canvas.SetTop(control, offsetY + 1);
@@ -54,7 +65,7 @@ namespace EPSPWPFClient.Tsunami
                 }
 
                 {
-                    var text = new TextBlock() { Text = message, FontSize = 24, Foreground = Brushes.White };
+                    var text = new TextBlock() { Text = cancelledMessage, FontSize = 18, Foreground = Brushes.White };
                     var control = new ContentControl() { Content = text };
                     Canvas.SetLeft(control, offsetX - 1);
                     Canvas.SetTop(control, offsetY);
@@ -62,6 +73,43 @@ namespace EPSPWPFClient.Tsunami
                 }
 
                 return;
+            }
+
+            // 津波予報（とりあえずテキスト出力しておく）
+            // TODO: あとで地図描画に対応させる
+            var stream = EventArgs.RegionList.GroupBy(e => e.Category).OrderByDescending(e => e.Key).Select(e =>
+            {
+                var categoryMessage = new StringBuilder();
+                categoryMessage.Append(
+                    new Dictionary<TsunamiCategory, string>() {
+                        { TsunamiCategory.MajorWarning, "【大津波警報】" },
+                        { TsunamiCategory.Warning, "【津波警報】" },
+                        { TsunamiCategory.Advisory, "【津波注意報】" },
+                        { TsunamiCategory.Unknown, "【不明】" },
+                    }[e.Key]);
+                categoryMessage.Append(Environment.NewLine);
+                categoryMessage.Append(
+                    string.Join("、", e.Select(f => f.Region + (f.IsImmediately ? "＊" : "")).ToList())
+                    );
+                return categoryMessage.ToString();
+            });
+
+            var message = string.Join(Environment.NewLine, stream.ToList());
+
+            {
+                var text = new TextBlock() { Text = message, FontSize = 18, Foreground = Brushes.Black, TextWrapping = System.Windows.TextWrapping.Wrap, Width = img.ActualWidth };
+                var control = new ContentControl() { Content = text };
+                Canvas.SetLeft(control, offsetX + 0);
+                Canvas.SetTop(control, offsetY + 1);
+                canvas.Children.Add(control);
+            }
+
+            {
+                var text = new TextBlock() { Text = message, FontSize = 18, Foreground = Brushes.White, TextWrapping = System.Windows.TextWrapping.Wrap, Width = img.ActualWidth };
+                var control = new ContentControl() { Content = text };
+                Canvas.SetLeft(control, offsetX - 1);
+                Canvas.SetTop(control, offsetY);
+                canvas.Children.Add(control);
             }
         }
     }
