a1a6f3c6328f610db493736c534194d130be7457
p2pquake-takuya <takuya@p2pquake.net>
2018-10-07 23:31:04 +0900

    Implement MongoDBReader read function

---
+++ UserquakeSimulator/Reader/MongoDBReader.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserquakeSimulator.Reader
+{
+    class MongoDBReader : IReader
+    {
+        private MongoClient client;
+        private IMongoCollection<BsonDocument> collection;
+
+        public MongoDBReader(MongoClient client, IMongoCollection<BsonDocument> collection)
+        {
+            this.client = client;
+            this.collection = collection;
+        }
+
+        public IEnumerable<EPSPData> GetAll()
+        {
+            var builder = Builders<BsonDocument>.Filter;
+            var filter = builder.Eq("code", 555) | builder.Eq("code", 561);
+
+            foreach (BsonDocument document in collection.Find(filter).ToEnumerable())
+            {
+
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
