c26e737bcbec7d7c0473eea3c0eeb4a6512a8c4c
p2pquake-takuya <takuya@p2pquake.net>
2018-10-31 00:05:04 +0900

    Add Extended UQManager to evaluate accuracy

---
+++ UserquakeSimulator/Evaluator/IUQExtManager.cs
@@ -0,0 +1,21 @@
+﻿using Client.Misc.UQSummary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserquakeSimulator.Evaluator
+{
+    public class UQDataEventArgs : EventArgs
+    {
+        public IList<Userquake> List { get; set; }
+        public IDictionary<string, int> Summary { get; set; }
+        public bool IsSatisfied { get; set; }
+    }
+
+    public interface IUQExtManager : IUQManager
+    {
+        event EventHandler<UQDataEventArgs> Initialized;
+    }
+}
---
+++ UserquakeSimulator/Evaluator/UQExtManager.cs
@@ -0,0 +1,36 @@
+﻿using Client.Misc.UQSummary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserquakeSimulator.Evaluator
+{
+    public class UQExtManager : UQManager, IUQExtManager
+    {
+        public event EventHandler<UQDataEventArgs> Initialized;
+
+        public UQExtManager()
+        {
+            Interval = 40;
+        }
+
+        public override void Add(string areaCode)
+        {
+            if (userquakeList.Count > 0 &&
+                ProtocolTime().Subtract(userquakeList.Last().AbstractTime).TotalSeconds >= Interval)
+            {
+                var eventArgs = new UQDataEventArgs()
+                {
+                    List = new List<Userquake>(userquakeList),
+                    Summary = GetCurrentSummary(),
+                    IsSatisfied = isOnGoing
+                };
+                Initialized(this, eventArgs);
+            }
+
+            base.Add(areaCode);
+        }
+    }
+}
