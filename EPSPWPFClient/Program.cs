0fa8ecf51941ad2e75e236141de7b915d78b249c
p2pquake-takuya <takuya@p2pquake.net>
2018-12-02 17:08:09 +0900

    Clean using

--- EPSPWPFClient/Program.cs
+++ EPSPWPFClient/Program.cs
@@ -1,10 +1,6 @@
 ﻿using EPSPWPFClient.Mediator;
 using Reactive.Bindings;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EPSPWPFClient
 {
