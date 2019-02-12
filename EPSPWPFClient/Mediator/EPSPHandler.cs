c36bfde313fb291654ec3fdedc4c314a666b1a1b
typewriter <takuyaxp@gmail.com>
2019-02-12 22:28:27 +0900

    Support userquake event handling (incomplete)

--- EPSPWPFClient/Mediator/EPSPHandler.cs
+++ EPSPWPFClient/Mediator/EPSPHandler.cs
@@ -8,6 +8,7 @@ using System.Windows.Data;
 using ClientHelper.Misc.UQSummary;
 using Client.Peer;
 using log4net;
+using EPSPWPFClient.Userquake;
 
 namespace EPSPWPFClient.Mediator
 {
@@ -18,8 +19,20 @@ namespace EPSPWPFClient.Mediator
     {
         public ObservableCollection<EPSPDataEventArgs> EventList { get; } = new ObservableCollection<EPSPDataEventArgs>();
 
-        public EPSPHandler()
+        private IUQManager uqManager = new UQManager();
+        private IUQJudge uqJudge = new SimpleUQJudge(3);
+        private EPSPUQSummaryEventArgs headUQSummary;
+        private Func<DateTime> protocolTime;
+
+        public EPSPHandler(Func<DateTime> protocolTime, Func<IDictionary<string, int>> areaPeerDictionary)
         {
+            this.protocolTime = protocolTime;
+            uqManager.ProtocolTime = protocolTime;
+            uqManager.AreaPeerDictionary = areaPeerDictionary;
+            uqManager.UQJudge = uqJudge;
+            uqManager.Occurred += UqManager_Occurred;
+            uqManager.Updated += UqManager_Updated;
+
             BindingOperations.EnableCollectionSynchronization(EventList, new object());
         }
 
@@ -53,6 +66,35 @@ namespace EPSPWPFClient.Mediator
             if (e.IsTest) { return; }
 
             EventList.Add(e);
+        }
+
+        /// <summary>
+        /// 地震感知情報のイベント処理
+        /// </summary>
+        internal void MediatorContext_OnUserquake(object sender, EPSPUserquakeEventArgs e)
+        {
+            if (!e.IsValid) { return; }
+
+            uqManager.Add(e.AreaCode);
+        }
+
+        private void UqManager_Updated(object sender, EventArgs e)
+        {
+            headUQSummary.UpdatedAt = protocolTime();
+            headUQSummary.Summary = uqManager.GetCurrentSummary();
+
+            // FIXME: Notify EventList subscriber
+        }
+
+        private void UqManager_Occurred(object sender, EventArgs e)
+        {
+            headUQSummary = new EPSPUQSummaryEventArgs()
+            {
+                Summary = uqManager.GetCurrentSummary(),
+                StartedAt = protocolTime(),
+                UpdatedAt = protocolTime()
+            };
+            EventList.Add(headUQSummary);
         }
     }
 }
---
+++ EPSPWPFClient/Userquake/EPSPUQSummaryEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using Client.Peer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPSPWPFClient.Userquake
+{
+    public class EPSPUQSummaryEventArgs : EPSPDataEventArgs
+    {
+        public DateTime StartedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public IDictionary<string, int> Summary { get; set; }
+    }
+}
