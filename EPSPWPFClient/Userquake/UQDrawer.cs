576c4b8fff8fdd7fa08ebc99a5c5e02710588b76
typewriter <takuyaxp@gmail.com>
2019-05-01 17:26:43 +0900

    Change to draw Userquake as a circle

--- EPSPWPFClient/Userquake/UQDrawer.cs
+++ EPSPWPFClient/Userquake/UQDrawer.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace EPSPWPFClient.Userquake
 {
@@ -56,26 +57,33 @@ namespace EPSPWPFClient.Userquake
                 var xy = calculator.calculate(point[0], point[1]);
 
                 var borderColor = Brushes.Black;
-                var bgColor = new SolidColorBrush(Color.FromArgb(255, 230, 204, 255));
 
-                var control = new ContentControl()
+                var radius = CalcRadiusRate(kv.Key, value, null, dictionary) * 64 + 5;
+                var bgColor = new SolidColorBrush(Color.FromArgb((byte)(128 + radius * 2), 230, 204, 255));
+
+                var ellipse = new ContentControl()
                 {
-                    Content = new Border()
+                    Content = new Ellipse()
                     {
-                        BorderBrush = borderColor,
-                        BorderThickness = new System.Windows.Thickness(1),
-                        Child = new TextBlock()
-                        {
-                            Text = value.ToString(),
-                            FontSize = 12,
-                            Foreground = Brushes.Black,
-                            Background = bgColor,
-                        },
-                    }
+                        Fill = bgColor,
+                        Width = radius,
+                        Height = radius,
+                    },
                 };
-                Canvas.SetLeft(control, xy[0] + offsetX - 8);
-                Canvas.SetTop(control, xy[1] + offsetY - 8);
-                canvas.Children.Add(control);
+                var textBlock = new TextBlock()
+                {
+                    Text = value.ToString(),
+                    FontSize = 12,
+                    Foreground = Brushes.Black
+                };
+                Canvas.SetLeft(ellipse, xy[0] + offsetX - radius / 2);
+                Canvas.SetTop(ellipse, xy[1] + offsetY - radius / 2);
+                Canvas.SetZIndex(ellipse, 3);
+                canvas.Children.Add(ellipse);
+                Canvas.SetLeft(textBlock, xy[0] + offsetX - 8);
+                Canvas.SetTop(textBlock, xy[1] + offsetY - 8);
+                Canvas.SetZIndex(textBlock, 4);
+                canvas.Children.Add(textBlock);
             }
         }
 
@@ -94,6 +102,14 @@ namespace EPSPWPFClient.Userquake
                 var elements = line.Split(',');
                 areaPoints.Add(elements[0], new double[] { double.Parse(elements[4]), double.Parse(elements[5]) });
             }
+        }
+
+        private static double CalcRadiusRate(string areaCode, int peer, IDictionary<string, int> peerMap, IDictionary<string, int> userquakeMap)
+        {
+            return Math.Min(1.0, Math.Min(
+                1.0 * peer / 10,
+                1.0 //1.0 * peer / (peerMap.ContainsKey(areaCode) ? peerMap[areaCode] : 1) * 10
+                ));
         }
     }
 }
