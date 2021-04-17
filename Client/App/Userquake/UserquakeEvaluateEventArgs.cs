0fcf5333d27612c0c1a1a979ad267b3f60743950
typewriter <takuyaxp@gmail.com>
2021-04-17 14:50:04 +0900

    Implement UserquakeEvaluateEventArgs

--- Client/App/Userquake/UserquakeEvaluateEventArgs.cs
+++ Client/App/Userquake/UserquakeEvaluateEventArgs.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Client.App.Userquake
 {
+    /// <summary>
+    /// 地震感知情報の評価結果
+    /// </summary>
     public interface IUserquakeEvaluation
     {
         /// <summary>開始日時</summary>
@@ -14,8 +17,55 @@ namespace Client.App.Userquake
         DateTime UpdatedAt { get; }
         /// <summary>件数</summary>
         int Count { get; }
-        /// <summary>信頼度 (適合率)</summary>
+        /// <summary>
+        /// P2P地震情報 Beta3 互換の信頼度 (適合率)
+        ///
+        /// <list type="bullet">
+        /// <item><term>0</term><description>非表示</description></item>
+        /// <item><term>0.97015</term><description>レベル1</description></item>
+        /// <item><term>0.96774</term><description>レベル2</description></item>
+        /// <item><term>0.97024</term><description>レベル3</description></item>
+        /// <item><term>0.98052</term><description>レベル4</description></item>
+        /// </list>
+        /// </summary>
         double Confidence { get; }
+        /// <summary>
+        /// P2P地震情報 Beta3 互換の信頼度レベル (1～4、非表示は 0)
+        /// </summary>
+        int ConfidenceLevel { get; }
+
+        /// <summary>
+        /// 地域毎の信頼度情報
+        /// </summary>
+        IReadOnlyDictionary<string, IUserquakeEvaluationArea> AreaConfidences { get; }
+    }
+
+    /// <summary>
+    /// 地震感知情報 地域の信頼度情報
+    /// </summary>
+    public interface IUserquakeEvaluationArea
+    {
+        /// <summary>地域コード</summary>
+        string AreaCode { get; }
+        /// <summary>件数</summary>
+        int Count { get; }
+        /// <summary>
+        /// P2P地震情報 Beta3 互換の信頼度
+        ///
+        /// <list type="bullet">
+        /// <item><term>0未満</term><description>信頼度 F</description></item>
+        /// <item><term>0.0以上0.2未満</term><description>信頼度 E</description></item>
+        /// <item><term>0.2以上0.4未満</term><description>信頼度 D</description></item>
+        /// <item><term>0.4以上0.6未満</term><description>信頼度 C</description></item>
+        /// <item><term>0.6以上0.8未満</term><description>信頼度 B</description></item>
+        /// <item><term>0.8以上</term><description>信頼度 A</description></item>
+        /// </list>
+        /// </summary>
+        double Confidence { get; }
+        /// <summary>
+        /// P2P地震情報 Beta3 互換の信頼度ラベル (A～F)
+        /// </summary>
+        string ConfidenceLevel { get; }
     }
 
     public class UserquakeEvaluateEventArgs : EventArgs, IUserquakeEvaluation
@@ -24,13 +74,39 @@ namespace Client.App.Userquake
         public DateTime UpdatedAt { get; private set; }
         public int Count { get; private set; }
         public double Confidence { get; private set; }
+        public int ConfidenceLevel => Confidence switch
+        {
+            double x when x >= 0.9805 => 4,
+            double x when x >= 0.9702 => 3,
+            double x when x >= 0.9701 => 1, // 間違っていない. 2 のほうが適合率が低い
+            double x when x >= 0.9677 => 2,
+            _ => 0,
+        };
+        public IReadOnlyDictionary<string, IUserquakeEvaluationArea> AreaConfidences { get; private set; }
 
-        public UserquakeEvaluateEventArgs(DateTime startedAt, DateTime updatedAt, int count, double confidence)
+        public UserquakeEvaluateEventArgs(DateTime startedAt, DateTime updatedAt, int count, double confidence, IReadOnlyDictionary<string, IUserquakeEvaluationArea> areaConfidences)
         {
             StartedAt = startedAt;
             UpdatedAt = updatedAt;
             Count = count;
             Confidence = confidence;
+            AreaConfidences = areaConfidences;
         }
+    }
+
+    public class UserquakeEvaluationArea : IUserquakeEvaluationArea
+    {
+        public string AreaCode { get; private set; }
+        public int Count { get; private set; }
+        public double Confidence { get; private set; }
+        public string ConfidenceLevel => Confidence switch
+        {
+            double x when x >= 0.8 => "A",
+            double x when x >= 0.6 => "B",
+            double x when x >= 0.4 => "C",
+            double x when x >= 0.2 => "D",
+            double x when x >= 0.0 => "E",
+            _ => "F",
+        };
     }
 }
---
+++ ClientTest/App/Userquake/UserquakeEvaluateEventArgsTest.cs
@@ -0,0 +1,67 @@
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTest.App.Userquake
+{
+    [TestFixture]
+    class UserquakeEvaluateEventArgsTest
+    {
+        [TestCase]
+        public void EvaluationConfidenceLevelMapTest()
+        {
+            var patterns = new KeyValuePair<double, int>[]
+            {
+                new KeyValuePair<double, int>(0.0, 0),
+                new KeyValuePair<double, int>(0.97015, 1),
+                new KeyValuePair<double, int>(0.96774, 2),
+                new KeyValuePair<double, int>(0.97024, 3),
+                new KeyValuePair<double, int>(0.98052, 4),
+            };
+
+            foreach (var pattern in patterns) {
+                var e = new Client.App.Userquake.UserquakeEvaluateEventArgs(
+                    DateTime.MinValue,
+                    DateTime.MinValue,
+                    0,
+                    pattern.Key,
+                    null
+                );
+
+                Assert.AreEqual(pattern.Value, e.ConfidenceLevel);
+            }
+        }
+
+        [TestCase]
+        public void AreaConfidenceLevelMapTest()
+        {
+            var patterns = new KeyValuePair<double, string>[]
+            {
+                new KeyValuePair<double, string>(-0.01, "F"),
+                new KeyValuePair<double, string>(0.0, "E"),
+                new KeyValuePair<double, string>(0.1999999, "E"),
+                new KeyValuePair<double, string>(0.2, "D"),
+                new KeyValuePair<double, string>(0.3999999, "D"),
+                new KeyValuePair<double, string>(0.4, "C"),
+                new KeyValuePair<double, string>(0.5999999, "C"),
+                new KeyValuePair<double, string>(0.6, "B"),
+                new KeyValuePair<double, string>(0.7999999, "B"),
+                new KeyValuePair<double, string>(0.8, "A"),
+            };
+
+            foreach (var pattern in patterns)
+            {
+                var e = new Client.App.Userquake.UserquakeEvaluationArea();
+                var prop = e.GetType().GetProperty("Confidence", BindingFlags.Public | BindingFlags.GetProperty | BindingFlags.Instance);
+                prop.SetValue(e, pattern.Key);
+
+                Assert.AreEqual(pattern.Value, e.ConfidenceLevel);
+            }
+        }
+    }
+}
