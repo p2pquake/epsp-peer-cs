fcc2b6d631247d7a1f7b8a274e743c5948bc66b7
typewriter <takuyaxp@gmail.com>
2021-04-24 17:04:52 +0900

    Create CLI project

---
+++ CLI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Client.App;
+
+using log4net.Config;
+
+using System;
+
+namespace CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+
+            var mc = new MediatorContext();
+            mc.Connect();
+
+            Console.ReadLine();
+        }
+    }
+}
