93319bb8916929996bf6d4a1bfb1b9dd43e1bb6d
typewriter <takuyaxp@gmail.com>
2021-05-01 22:25:21 +0900

    Support gRPC server

--- CLI/Observers/DummyObserver.cs
+++ CLI/Observers/DummyObserver.cs
@@ -3,69 +3,56 @@ using Client.App.Userquake;
 using Client.Client;
 using Client.Peer;
 
-using log4net;
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CLI.Receiver
+namespace CLI.Observers
 {
-    class PrintReceiver : IReceiver
+    public class DummyObserver : IObserver
     {
-        private readonly ILog log = LogManager.GetLogger("PrintReceiver");
         public MediatorContext MediatorContext { private get; init; }
 
         public void Completed(object sender, OperationCompletedEventArgs e)
         {
-            log.Info($"Completed. s: {sender}, e: {ObjectDumper.Dump(e)}");
         }
 
         public void ConnectionsChanged(object sender, EventArgs e)
         {
-            log.Info($"ConnectionsChanged. s: {sender}, e: {ObjectDumper.Dump(e)}");
         }
 
         public void OnAreapeers(object sender, EventArgs e)
         {
-            log.Info($"OnAreapeers. s: {sender}, e: {ObjectDumper.Dump(e)}");
         }
 
         public void OnEarthquake(object sender, EPSPQuakeEventArgs e)
         {
-            log.Info($"OnEarthquake. s: {sender}, e: {ObjectDumper.Dump(e)}");
         }
 
         public void OnEEWTest(object sender, EPSPEEWTestEventArgs e)
         {
-            log.Info($"OnEEWTest. s: {sender}, e: {ObjectDumper.Dump(e)}");
         }
 
         public void OnNewUserquakeEvaluation(object sender, UserquakeEvaluateEventArgs e)
         {
-            log.Info($"OnNewUserquakeEvaluation. s: {sender}, e: {ObjectDumper.Dump(e)}");
         }
 
         public void OnTsunami(object sender, EPSPTsunamiEventArgs e)
         {
-            log.Info($"OnTsunami. s: {sender}, e: {ObjectDumper.Dump(e)}");
         }
 
         public void OnUpdateUserquakeEvaluation(object sender, UserquakeEvaluateEventArgs e)
         {
-            log.Info($"OnUpdateUserquakeEvaluation. s: {sender}, e: {ObjectDumper.Dump(e)}");
         }
 
         public void OnUserquake(object sender, EPSPUserquakeEventArgs e)
         {
-            log.Info($"OnUserquake. s: {sender}, e: {ObjectDumper.Dump(e)}");
         }
 
         public void StateChanged(object sender, EventArgs e)
         {
-            log.Info($"StateChanged. s: {sender}, e: {ObjectDumper.Dump(e)}");
         }
     }
 }
