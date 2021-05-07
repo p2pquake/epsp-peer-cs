151ad725a017ed9b0f8b214c7232c1b1b48ff80c
typewriter <takuyaxp@gmail.com>
2021-05-07 18:10:25 +0900

    Use resource files

--- Map/Program.cs
+++ Map/Program.cs
@@ -4,7 +4,7 @@ using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.Fonts;
 
 using System;
-
+using System.IO;
 
 namespace MapSample
 {
@@ -13,13 +13,13 @@ namespace MapSample
         static void Main(string[] args)
         {
             var fc = new FontCollection();
-            var ff = fc.Install(@"..\..\..\Resources\Fonts\RobotoMono-Regular.ttf");
-            var ffb = fc.Install(@"..\..\..\Resources\Fonts\RobotoMono-Bold.ttf");
+            var ff = fc.Install(new MemoryStream(Map.FontResource.RobotoMono_Regular));
+            var ffb = fc.Install(new MemoryStream(Map.FontResource.RobotoMono_Bold));
             var fb = ffb.CreateFont(16, FontStyle.Bold);
             var f = ff.CreateFont(16);
 
-            using var image = Image.Load(@"..\..\..\Resources\Maps\japan-gsi_1024-8bit.png");
-            using var description = Image.Load(@"..\..\..\Resources\Images\description.png");
+            using var image = Image.Load(new MemoryStream(Map.ImageResource.japan_gsi_1024));
+            using var description = Image.Load(new MemoryStream(Map.ImageResource.description));
 
             var rect = new Rectangle(450, 350, 180, 60);
             image.Mutate(x => x.Fill(Color.WhiteSmoke, rect));
