e56642cd74af0afb9162983da24ea2d93964ff8c
typewriter <takuyaxp@gmail.com>
2021-07-22 17:16:26 +0900

    Add observation areas model

---
+++ Map/Model/GeoJson.cs
@@ -0,0 +1,28 @@
+﻿namespace Map.Model
+{
+    public class GeoJson
+    {
+        public string type { get; set; }
+        public Feature[] features { get; set; }
+    }
+
+    public class Feature
+    {
+        public string type { get; set; }
+        public Geometry geometry { get; set; }
+        public Properties properties { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string type { get; set; }
+        public float[][][][] coordinates { get; set; }
+    }
+
+    public class Properties
+    {
+        public string code { get; set; }
+        public bool islandBold { get; set; }
+        public object name { get; set; }
+    }
+}
---
+++ Map/Model/ObservationAreas.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace Map.Model
+{
+    public record ObservationArea(string Prefecture, string Name, GeoCoordinate[][] Coordinates);
+
+    public class ObservationAreas
+    {
+        public static ObservationAreas Instance { get; } = new();
+
+        private readonly IReadOnlyDictionary<string, ObservationArea> observationAreas;
+
+        private ObservationAreas()
+        {
+            Dictionary<string, ObservationArea> areas = new();
+
+            var jsons = JsonSerializer.Deserialize<GeoJson>(PointResource.AreaGeoJsons, new JsonSerializerOptions { IncludeFields = true });
+            var areaCodes = PointResource.EarthquakeAreaCodes.Replace("\r", "").Split('\n').Where((line) => line.Length > 0).Select((line) => line.Split(',')).ToDictionary(e => e[0], e => e[1]);
+
+            foreach (var feature in jsons.features)
+            {
+                var name = areaCodes[feature.properties.code];
+                var area = new ObservationArea(
+                    null,
+                    name,
+                    feature.geometry.coordinates
+                    .Select(e => e[0].Select(e => new GeoCoordinate(e[1], e[0])).ToArray()).ToArray()
+                    );
+
+                // 既に存在してたら足す
+                if (areas.ContainsKey(name))
+                {
+                    areas[name] = new ObservationArea(
+                        null,
+                        name,
+                        areas[name].Coordinates.Concat(area.Coordinates).ToArray()
+                        );
+                } else
+                {
+                    areas.Add(name, area);
+                }
+
+            }
+
+            observationAreas = areas;
+        }
+
+        public ObservationArea GetArea(string name)
+        {
+            if (!observationAreas.ContainsKey(name))
+            {
+                return null;
+            }
+
+            return observationAreas[name];
+        }
+    }
+}
