6aec8dd21a092cb2f997ca55709eb2334b038fc6
typewriter <takuyaxp@gmail.com>
2021-10-06 02:26:08 +0900

    Save settings in JSON

---
+++ WpfClient/Configuration.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WpfClient
+{
+    public class ConfigurationManager
+    {
+        private static readonly string fileName = "example.json";
+
+        private static Configuration configuration;
+        public static Configuration Configuration
+        {
+            get
+            {
+                if (configuration == null)
+                {
+                    configuration = Load();
+                }
+
+                return configuration;
+            }
+        }
+
+        public static bool Save()
+        {
+            try
+            {
+                string json = JsonSerializer.Serialize(Configuration);
+                File.WriteAllText(fileName, json);
+                return true;
+            }
+            catch (Exception)
+            {
+                // FIXME: エラーハンドリングする
+                return false;
+            }
+        }
+
+        private static Configuration Load()
+        {
+            if (!File.Exists(fileName))
+            {
+                return new Configuration();
+            }
+
+            try
+            {
+                string json = File.ReadAllText(fileName);
+                return JsonSerializer.Deserialize<Configuration>(json);
+            }
+            catch (Exception)
+            {
+                // FIXME: エラーハンドリングする
+                return new Configuration();
+            }
+        }
+    }
+
+    public class Notification
+    {
+        public bool Enabled { get; set; }
+        public bool Show { get; set; }
+        public bool Notice { get; set; }
+        public bool Sound { get; set; }
+    }
+
+    public class EarthquakeNotification : Notification
+    {
+        public int MinScale { get; set; } = 10;
+    }
+
+    public class Configuration
+    {
+        // 起動
+        public bool BootAtStartup { get; set; } = false;
+        public bool MinimizeAtBoot { get; set; } = false;
+
+        // 接続
+        public bool PortOpen { get; set; } = true;
+        public bool UseUPnP { get; set; } = true;
+        public int Port { get; set; } = 6911;
+
+        // 「揺れた！」
+        public string Area { get; set; } = "901";
+        public bool SendIfMiddleDoubleClick { get; set; } = false;
+        public bool SendIfRightDoubleClick { get; set; } = false;
+
+        // 表示・通知
+        public EarthquakeNotification EarthquakeNotification { get; set; } = new EarthquakeNotification();
+        public Notification UserquakeNotification { get; set; } = new Notification();
+        public Notification TsunamiNotification { get; set; } = new Notification();
+        public Notification EEWTestNotification { get; set; } = new Notification();
+    }
+}
--- WpfClient/SettingViewModel.cs
+++ WpfClient/SettingViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -12,6 +13,20 @@ namespace WpfClient
     public class SettingViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        // FIXME: 震度の変換テーブルが Program.cs にもある。後で整理したい
+        private string[][] earthquakeScaleTable = new string[][]
+        {
+            new string[] { "10", "1" },
+            new string[] { "20", "2" },
+            new string[] { "30", "3" },
+            new string[] { "40", "4" },
+            new string[] { "45", "5弱" },
+            new string[] { "50", "5強" },
+            new string[] { "55", "6弱" },
+            new string[] { "60", "6強" },
+            new string[] { "70", "7" },
+        };
 
         private string selectTag;
         public string SelectTag
@@ -47,26 +62,379 @@ namespace WpfClient
             get { return selectTag == "Notification" ? Visibility.Visible : Visibility.Hidden; }
         }
 
-        public bool BootAtStartup { get; set; }
-        public bool MinimizeAtBoot { get; set; }
+        private bool bootAtStartup;
+        public bool BootAtStartup
+        {
+            get => bootAtStartup;
+            set
+            {
+                bootAtStartup = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool minimizeAtBoot;
+        public bool MinimizeAtBoot
+        {
+            get => minimizeAtBoot;
+            set
+            {
+                minimizeAtBoot = value;
+                OnPropertyChanged();
+            }
+        }
 
         private bool portOpen;
         public bool PortOpen
         {
-            get { return portOpen; }
+            get => portOpen;
             set
             {
                 portOpen = value;
                 OnPropertyChanged();
             }
         }
-        public bool UseUPnP { get; set; }
-        public int Port { get; set; }
+
+        private bool useUPnP;
+        public bool UseUPnP
+        {
+            get => useUPnP;
+            set
+            {
+                useUPnP = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int port;
+        public int Port
+        {
+            get => port;
+            set
+            {
+                port = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> UserquakeAreas =>
+            new ObservableCollection<string>(Resource.epsp_area.Split('\n').Skip(1).Select(e => e.Split(',')[4]));
+
+        private string selectArea;
+        public string SelectArea
+        {
+            get => selectArea;
+            set
+            {
+                selectArea = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool sendIfMiddleDoubleClick;
+        public bool SendIfMiddleDoubleClick
+        {
+            get => sendIfMiddleDoubleClick;
+            set
+            {
+                sendIfMiddleDoubleClick = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool sendIfRightDoubleClick;
+        public bool SendIfRightDoubleClick
+        {
+            get => sendIfRightDoubleClick;
+            set
+            {
+                sendIfRightDoubleClick = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool earthquakeNotification;
+        public bool EarthquakeNotification
+        {
+            get => earthquakeNotification;
+            set
+            {
+                earthquakeNotification = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> EarthquakeScales =>
+            new ObservableCollection<string>(earthquakeScaleTable.Select(e => $"震度 {e[1]} 以上"));
+
+        private string earthquakeMinScale;
+        public string EarthquakeMinScale
+        {
+            get => earthquakeMinScale;
+            set
+            {
+                earthquakeMinScale = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool earthquakeShow;
+        public bool EarthquakeShow
+        {
+            get => earthquakeShow;
+            set
+            {
+                earthquakeShow = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool earthquakeNotice;
+        public bool EarthquakeNotice
+        {
+            get => earthquakeNotice;
+            set
+            {
+                earthquakeNotice = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool earthquakeSound;
+        public bool EarthquakeSound
+        {
+            get => earthquakeSound;
+            set
+            {
+                earthquakeSound = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool userquakeNotification;
+        public bool UserquakeNotification
+        {
+            get => userquakeNotification;
+            set
+            {
+                userquakeNotification = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool userquakeShow;
+        public bool UserquakeShow
+        {
+            get => userquakeShow;
+            set
+            {
+                userquakeShow = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool userquakeNotice;
+        public bool UserquakeNotice
+        {
+            get => userquakeNotice;
+            set
+            {
+                userquakeNotice = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool userquakeSound;
+        public bool UserquakeSound
+        {
+            get => userquakeSound;
+            set
+            {
+                userquakeSound = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool tsunamiNotification;
+        public bool TsunamiNotification
+        {
+            get => tsunamiNotification;
+            set
+            {
+                tsunamiNotification = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool tsunamiShow;
+        public bool TsunamiShow
+        {
+            get => tsunamiShow;
+            set
+            {
+                tsunamiShow = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool tsunamiNotice;
+        public bool TsunamiNotice
+        {
+            get => tsunamiNotice;
+            set
+            {
+                tsunamiNotice = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool tsunamiSound;
+        public bool TsunamiSound
+        {
+            get => tsunamiSound;
+            set
+            {
+                tsunamiSound = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool eewTestNotification;
+        public bool EEWTestNotification
+        {
+            get => eewTestNotification;
+            set
+            {
+                eewTestNotification = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool eewTestShow;
+        public bool EEWTestShow
+        {
+            get => eewTestShow;
+            set
+            {
+                eewTestShow = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool eewTestNotice;
+        public bool EEWTestNotice
+        {
+            get => eewTestNotice;
+            set
+            {
+                eewTestNotice = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool eewTestSound;
+        public bool EEWTestSound
+        {
+            get => eewTestSound;
+            set
+            {
+                eewTestSound = value;
+                OnPropertyChanged();
+            }
+        }
+            
+        public void LoadFromConfiguration(Configuration configuration)
+        {
+            bootAtStartup = configuration.BootAtStartup;
+            minimizeAtBoot = configuration.MinimizeAtBoot;
+
+            portOpen = configuration.PortOpen;
+            useUPnP = configuration.UseUPnP;
+            port = configuration.Port;
+
+            selectArea = configuration.Area;
+            sendIfMiddleDoubleClick = configuration.SendIfMiddleDoubleClick;
+            sendIfRightDoubleClick = configuration.SendIfRightDoubleClick;
+
+            // XXX: この羅列… なんとかならんのか？
+            earthquakeNotification = configuration.EarthquakeNotification.Enabled;
+            earthquakeMinScale = ConvertScaleValueToLabel(configuration.EarthquakeNotification.MinScale);
+            earthquakeShow = configuration.EarthquakeNotification.Show;
+            earthquakeNotice = configuration.EarthquakeNotification.Notice;
+            earthquakeSound = configuration.EarthquakeNotification.Sound;
+
+            userquakeNotification = configuration.UserquakeNotification.Enabled;
+            userquakeShow = configuration.UserquakeNotification.Show;
+            userquakeNotice = configuration.UserquakeNotification.Notice;
+            userquakeSound = configuration.UserquakeNotification.Sound;
+
+            tsunamiNotification = configuration.TsunamiNotification.Enabled;
+            tsunamiShow = configuration.TsunamiNotification.Show;
+            tsunamiNotice = configuration.TsunamiNotification.Notice;
+            tsunamiSound = configuration.TsunamiNotification.Sound;
+
+            eewTestNotification = configuration.EEWTestNotification.Enabled;
+            eewTestShow = configuration.EEWTestNotification.Show;
+            eewTestNotice = configuration.EEWTestNotification.Notice;
+            eewTestSound = configuration.EEWTestNotification.Sound;
+        }
+
+        private void WritebackConfiguration()
+        {
+            var configuration = ConfigurationManager.Configuration;
+
+            configuration.BootAtStartup = bootAtStartup;
+            configuration.MinimizeAtBoot = minimizeAtBoot;
+
+            configuration.PortOpen = portOpen;
+            configuration.UseUPnP = useUPnP;
+            configuration.Port = port;
+
+            configuration.Area = selectArea;
+            configuration.SendIfMiddleDoubleClick = sendIfMiddleDoubleClick;
+            configuration.SendIfRightDoubleClick = sendIfRightDoubleClick;
+
+            configuration.EarthquakeNotification.Enabled = earthquakeNotification;
+            configuration.EarthquakeNotification.MinScale = ConvertScaleLabelToValue(earthquakeMinScale);
+            configuration.EarthquakeNotification.Show = earthquakeShow;
+            configuration.EarthquakeNotification.Notice = earthquakeNotice;
+            configuration.EarthquakeNotification.Sound = earthquakeSound;
+
+            configuration.UserquakeNotification.Enabled = userquakeNotification;
+            configuration.UserquakeNotification.Show = userquakeShow;
+            configuration.UserquakeNotification.Notice = userquakeNotice;
+            configuration.UserquakeNotification.Sound = userquakeSound;
+
+            configuration.TsunamiNotification.Enabled = tsunamiNotification;
+            configuration.TsunamiNotification.Show = tsunamiShow;
+            configuration.TsunamiNotification.Notice = tsunamiNotice;
+            configuration.TsunamiNotification.Sound = tsunamiSound;
+
+            configuration.EEWTestNotification.Enabled = eewTestNotification;
+            configuration.EEWTestNotification.Show = eewTestShow;
+            configuration.EEWTestNotification.Notice = eewTestNotice;
+            configuration.EEWTestNotification.Sound = eewTestSound;
+
+            ConfigurationManager.Save();
+        }
+
+        private int ConvertScaleLabelToValue(string scaleLabel)
+        {
+            var simplifyScaleLabel = scaleLabel.Replace("震度", "").Replace("以上", "").Replace(" ", "");
+            return int.Parse(earthquakeScaleTable.First(e => e[1] == simplifyScaleLabel)[0]);
+        }
+
+        private string ConvertScaleValueToLabel(int scale)
+        {
+            var simplifyScaleLabel = earthquakeScaleTable.First(e => int.Parse(e[0]) == scale)[1];
+            return $"震度 {simplifyScaleLabel} 以上";
+        }
 
         // See: https://docs.microsoft.com/ja-jp/dotnet/desktop/wpf/data/how-to-implement-property-change-notification
         private void OnPropertyChanged([CallerMemberName] string name = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            WritebackConfiguration();
         }
     }
 }
