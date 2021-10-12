92e8a5d3e76363b815d0b41fe23017b84bd746bc
typewriter <takuyaxp@gmail.com>
2021-10-12 20:34:10 +0900

    Add Tsunami, EEWTest and Unknown information view

--- WpfClient/EPSPDataView/EPSPTsunamiView.cs
+++ WpfClient/EPSPDataView/EPSPTsunamiView.cs
@@ -34,6 +34,7 @@ namespace WpfClient.EPSPDataView
         }
 
         public string Time => EventArgs.ReceivedAt.ToString("dd日HH時mm分");
+        public string DetailTime => EventArgs.ReceivedAt.ToString("dd日HH時mm分ss秒");
 
         public string Caption
         {
@@ -44,13 +45,7 @@ namespace WpfClient.EPSPDataView
                     return "津波予報 解除";
                 }
 
-                return MaxTsunamiCategory() switch
-                {
-                    TsunamiCategory.MajorWarning => "大津波警報",
-                    TsunamiCategory.Warning => "津波警報",
-                    TsunamiCategory.Advisory => "津波注意報",
-                    _ => "津波予報"
-                };
+                return TsunamiCategoryString(MaxTsunamiCategory());
             }
         }
 
@@ -64,5 +59,78 @@ namespace WpfClient.EPSPDataView
             if (categories.Contains(TsunamiCategory.Advisory)) { return TsunamiCategory.Advisory; }
             return TsunamiCategory.Unknown;
         }
+        public List<DetailItemView> DetailItemViewList
+        {
+            get
+            {
+                var list = new List<DetailItemView>();
+                if (EventArgs is null)
+                {
+                    return list;
+                }
+
+                list.Add(new DetailItemView($"津波予報 （受信日時： {DetailTime}）", TextStyles.Title));
+                if (EventArgs.IsCancelled)
+                {
+                    list.Add(new DetailItemView("津波予報はすべて解除されました。", TextStyles.Name));
+                    return list;
+                }
+
+                var regionsByCategories = EventArgs.RegionList.OrderBy(e => e.Category).Reverse().GroupBy(e => e.Category);
+                foreach (var regionsByCategory in regionsByCategories)
+                {
+                    list.Add(new DetailItemView(TsunamiCategoryDetailString(regionsByCategory.Key), TextStyles.Scale));
+                    list.Add(new DetailItemView(string.Join('、', regionsByCategory.Select(e => $"{(e.IsImmediately ? "＊" : "")}{e.Region}")), TextStyles.Name));
+                }
+
+                if (EventArgs.RegionList.Any(e => e.IsImmediately))
+                {
+                    list.Add(new DetailItemView("", TextStyles.Name));
+                    list.Add(new DetailItemView("＊印の沿岸では、ただちに津波が来襲すると予想されます", TextStyles.Name));
+                }
+
+                list.Add(new DetailItemView("", TextStyles.Name));
+                list.Add(new DetailItemView("とるべき行動（気象庁リーフレット「津波防災」より）", TextStyles.Prefecture));
+
+                if (EventArgs.RegionList.Any(e => e.Category == TsunamiCategory.MajorWarning))
+                {
+                    list.Add(new DetailItemView($"{TsunamiCategoryString(TsunamiCategory.MajorWarning)} ・ {TsunamiCategoryString(TsunamiCategory.Warning)}", TextStyles.Scale));
+                    list.Add(new DetailItemView("沿岸部や川沿いにいる人は、ただちに高台や避難ビルなど安全な場所へ避難してください。", TextStyles.Name));
+                    list.Add(new DetailItemView("津波は繰り返し襲ってくるので、大津波・津波警報が解除されるまで安全な場所から離れないでください。", TextStyles.Name));
+                    list.Add(new DetailItemView("＜ここなら安心と思わず、より高い場所を目指して避難しましょう！＞", TextStyles.Name));
+                } else if (EventArgs.RegionList.Any(e => e.Category == TsunamiCategory.Warning))
+                {
+                    list.Add(new DetailItemView(TsunamiCategoryString(TsunamiCategory.Warning), TextStyles.Scale));
+                    list.Add(new DetailItemView("沿岸部や川沿いにいる人は、ただちに高台や避難ビルなど安全な場所へ避難してください。", TextStyles.Name));
+                    list.Add(new DetailItemView("津波は繰り返し襲ってくるので、津波警報が解除されるまで安全な場所から離れないでください。", TextStyles.Name));
+                    list.Add(new DetailItemView("＜ここなら安心と思わず、より高い場所を目指して避難しましょう！＞", TextStyles.Name));
+                }
+
+                if (EventArgs.RegionList.Any(e => e.Category == TsunamiCategory.Advisory))
+                {
+                    list.Add(new DetailItemView(TsunamiCategoryString(TsunamiCategory.Advisory), TextStyles.Scale));
+                    list.Add(new DetailItemView("海の中にいる人は、ただちに海から上がって、海岸から離れてください。", TextStyles.Name));
+                    list.Add(new DetailItemView("津波注意報が解除されるまで海に入ったり海岸に近づいたりしないでください。", TextStyles.Name));
+                }
+
+                return list;
+            }
+        }
+
+        private static string TsunamiCategoryString(TsunamiCategory category) => category switch
+        {
+            TsunamiCategory.MajorWarning => "大津波警報",
+            TsunamiCategory.Warning => "津波警報",
+            TsunamiCategory.Advisory => "津波注意報",
+            _ => "津波予報"
+        };
+
+        private static string TsunamiCategoryDetailString(TsunamiCategory category) => category switch
+        {
+            TsunamiCategory.MajorWarning => "大津波警報（ 3 メートル以上）",
+            TsunamiCategory.Warning => "津波警報（最大 3 メートル）",
+            TsunamiCategory.Advisory => "津波注意報（最大 1 メートル）",
+            _ => "津波予報"
+        };
     }
 }
---
+++ WpfClient/Pages/Informations/EEWTest.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+using WpfClient.EPSPDataView;
+
+namespace WpfClient.Pages.Informations
+{
+    /// <summary>
+    /// EEWTest.xaml の相互作用ロジック
+    /// </summary>
+    public partial class EEWTest : Page
+    {
+        public EEWTest()
+        {
+            InitializeComponent();
+            this.DataContextChanged += EEWTest_DataContextChanged;
+            
+        }
+
+        private void EEWTest_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (this.DataContext is null) { return; }
+            var view = (EPSPEEWTestView)this.DataContext;
+            if (view.EventArgs is null) { return; }
+
+            var count = 0;
+            //if (DateTime.Now.Subtract(view.EventArgs.ReceivedAt).TotalSeconds > 30)
+            //{
+            //    count = int.MaxValue - 1;
+            //}
+
+            var timer = new DispatcherTimer(DispatcherPriority.Normal)
+            {
+                Interval = TimeSpan.FromSeconds(0.2),
+            };
+            timer.Tick += (s, e) =>
+            {
+                count++;
+                if (count > 150) {
+                    FlashMessage.Foreground = Brushes.Gray;
+                    timer.Stop();
+                    return;
+                }
+
+                if (count % 5 == 0)
+                {
+                    FlashMessage.Foreground = Brushes.Gray;
+                }
+                if (count % 5 == 1)
+                {
+                    FlashMessage.Foreground = Brushes.Red;
+                }
+            };
+            timer.Start();
+
+            Unloaded += (s, e) => timer.Stop();
+        }
+    }
+}
