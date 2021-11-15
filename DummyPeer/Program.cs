4b7a177b19379200dfd3fc304fbee9a4d1a4a8a0
typewriter <takuyaxp@gmail.com>
2021-11-16 01:20:44 +0900

    Create DummyPeer project

---
+++ DummyPeer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DummyPeer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
