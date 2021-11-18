3fa4cfdad3132a7a849170ba1bfe7657ad21f97b
typewriter <takuyaxp@gmail.com>
2021-11-18 22:48:08 +0900

    Add System.CommandLine

--- DummyPeer/Program.cs
+++ DummyPeer/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 
 namespace DummyPeer
 {
+    class RootOptions
+    {
+        public int Port { get; set; }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var root = new RootCommand()
+            {
+                new Option<int>(new[]{ "-p", "--port" }, () => 6910, "listen port")
+            };
+            root.Handler = CommandHandler.Create<RootOptions>(RootHandler);
+
+            return root.InvokeAsync(args).Result;
+        }
+
+        private static void RootHandler(RootOptions options)
+        {
+            Console.WriteLine($"port: {options.Port}");
+            Console.WriteLine("Hello world");
         }
     }
 }
