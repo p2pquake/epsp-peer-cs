4069bff2847b6a467f9097e62294b97285bc7f75
typewriter <takuyaxp@gmail.com>
2021-12-12 17:36:43 +0900

    Add license window

---
+++ WpfClient/LicenseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfClient
+{
+    public class LicenseViewModel
+    {
+        public ObservableCollection<LicenseModel> Licenses { get; init; }
+
+        public LicenseViewModel()
+        {
+            var licenseItems = License.ResourceManager.GetResourceSet(Thread.CurrentThread.CurrentCulture, true, true).OfType<DictionaryEntry>().Where(e => e.Value is string).OrderBy(e => e.Key).Select(e => new LicenseModel() { Name = (string)e.Key, Text = (string)e.Value });
+            Licenses = new ObservableCollection<LicenseModel>(licenseItems);
+        }
+    }
+
+    public class LicenseModel
+    {
+        public string Name { get; init; }
+        public string Text { get; init; }
+    }
+}
