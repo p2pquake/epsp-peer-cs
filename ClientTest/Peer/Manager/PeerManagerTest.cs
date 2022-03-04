57fa0ae581e3ffb1f5c060f30edb615ff55db396
typewriter <takuyaxp@gmail.com>
2022-03-04 20:57:21 +0900

    Change hop limit

--- ClientTest/Peer/Manager/PeerManagerTest.cs
+++ ClientTest/Peer/Manager/PeerManagerTest.cs
@@ -16,6 +16,59 @@ namespace ClientTest.Peer.Manager
     class PeerManagerTest
     {
         [TestCase]
+        public void PeerReadLineDuplicateRemoving()
+        {
+            var peerManager = new PeerManager()
+            {
+                PeerCount = () => 100,
+                PeerId = () => 1,
+                ProtocolTime = () => DateTime.Now,
+            };
+            peerManager.ConnectionsChanged += (s, e) => { };
+
+            var count = 0;
+            peerManager.OnAreapeers += (s, e) => { count++; };
+
+            var line = "561 1 ABC:2022/01/01 00-00-00:001,100";
+            var packet = Packet.Parse(line);
+            var eventArgs = new ReadLineEventArgs() { line = line, packet = packet };
+
+            var readLine = peerManager.GetType().GetMethod("Peer_ReadLine", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            Enumerable.Range(0, 5).ToList().ForEach(x => readLine.Invoke(peerManager, new object[] { null, eventArgs }));
+
+            Assert.AreEqual(1, count);
+        }
+
+        [TestCase]
+        public void PeerReadLineHopLimit()
+        {
+            var peerManager = new PeerManager()
+            {
+                PeerCount = () => 9,
+                PeerId = () => 1,
+                ProtocolTime = () => DateTime.Now,
+            };
+            peerManager.ConnectionsChanged += (s, e) => { };
+
+            var readLine = peerManager.GetType().GetMethod("Peer_ReadLine", BindingFlags.Instance | BindingFlags.NonPublic);
+            void fn(string line) => readLine.Invoke(peerManager, new object[] { null, new ReadLineEventArgs() { line = line, packet = Packet.Parse(line) } });    
+
+            var count = 0;
+            peerManager.OnAreapeers += (s, e) => { count++; };
+
+            fn("561 9 ABC:2022/01/01 00-00-09:001,100");
+            fn("561 10 ABC:2022/01/01 00-00-10:001,100");
+            fn("561 11 ABC:2022/01/01 00-00-11:001,100");
+            Assert.AreEqual(2, count);
+
+            peerManager.PeerCount = () => 400;
+            fn("561 20 ABC:2022/01/01 00-00-08:001,100");
+            fn("561 21 ABC:2022/01/01 00-00-09:001,100");
+            Assert.AreEqual(3, count);
+        }
+
+        [TestCase]
         public void DisconnectPeerWhileSendingPacket()
         {
             var peerManager = new PeerManager();
