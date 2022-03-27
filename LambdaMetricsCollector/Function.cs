790f5ba6fe00f76035722703be51e6a5842109d0
typewriter <takuyaxp@gmail.com>
2022-03-28 00:54:53 +0900

    Remove comment

--- LambdaMetricsCollector/Function.cs
+++ LambdaMetricsCollector/Function.cs
@@ -23,13 +23,6 @@ namespace LambdaMetricsCollector
 {
     public class Function
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
         public string FunctionHandler(object input, ILambdaContext context)
         {
             var servers = new string[]
