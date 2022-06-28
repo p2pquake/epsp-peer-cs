ba4d1af5703bf7be17d27134f9e979950cab0fac
typewriter <takuyaxp@gmail.com>
2022-06-28 11:36:51 +0900

    Add publisher HTTP server for sending EEW

--- CLI/Command/PublisherCommand.cs
+++ CLI/Command/PublisherCommand.cs
@@ -2,12 +2,20 @@
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
+
 using Client.App;
+
 using log4net;
 using log4net.Appender;
 using log4net.Config;
 using log4net.Layout;
+
+using Newtonsoft.Json;
 
 namespace CLI.Command
 {
@@ -56,12 +64,16 @@ namespace CLI.Command
             mc.Port = options.Port;
             mc.Connect();
 
+            var publishServer = new PublisherServer(mc, options.HttpPort, logger);
+            Task.Run(publishServer.Run);
+
             Console.WriteLine("Enter キーを押すと終了します。");
             Console.ReadLine();
 
             var sw = new Stopwatch();
             sw.Start();
             mc.Disconnect();
+            publishServer.Abort();
 
             while (sw.ElapsedMilliseconds <= 4000 && !mc.CanConnect)
             {
@@ -78,6 +90,54 @@ namespace CLI.Command
         private static void Mc_OnData(object sender, Client.Peer.EPSPRawDataEventArgs e)
         {
             logger.Info($"EPSP data arrived: {e.Packet}");
+        }
+    }
+
+    class PublisherServer
+    {
+        private IMediatorContext mc;
+        private int port;
+        private ILog logger;
+        private bool quit = false;
+
+        public PublisherServer(IMediatorContext mc, int port, ILog logger)
+        {
+            this.mc = mc;
+            this.port = port;
+            this.logger = logger;
+        }
+
+        public void Run()
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://*:" + port + "/");
+            listener.Start();
+
+            logger.Info($"HTTPServer listen {port}.");
+
+            while (!quit)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+
+                response.StatusCode = 200;
+                response.ContentType = "application/json";
+
+                string outputJson = JsonConvert.SerializeObject(mc.AreaPeerDictionary.Where(c => c.Key.Trim().Length == 3).ToDictionary(item => item.Key, item => item.Value));
+                byte[] outputBytes = Encoding.ASCII.GetBytes(outputJson);
+
+                response.OutputStream.Write(outputBytes, 0, outputBytes.Length);
+
+                response.Close();
+
+                logger.Info("Send areapeers");
+            }
+        }
+
+        public void Abort()
+        {
+            quit = true;
         }
     }
 }
