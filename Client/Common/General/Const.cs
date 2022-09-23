0e9eb74796e55ba2c3ed07e8591caebeff952d3b
typewriter <takuyaxp@gmail.com>
2022-09-23 15:35:19 +0900

    Set version 0.35.6

--- Client/Common/General/Const.cs
+++ Client/Common/General/Const.cs
@@ -7,10 +7,10 @@ namespace Client.Common.General
 {
     class Const
     {
-        public static readonly string PROTOCOL_VERSION = "0.35";
+        public static readonly string PROTOCOL_VERSION = "0.36";
         public static readonly string SOFTWARE_NAME = "P2PQ_SharpClient";
         // TODO: SOFTWARE_VERSIONを自動生成したい
-        public static readonly string SOFTWARE_VERSION = "compatible_0.35.004";
+        public static readonly string SOFTWARE_VERSION = "compatible_0.35.006";
         public static readonly double ALLOW_PROTOCOL_VERSION = 0.30;
     }
 }
