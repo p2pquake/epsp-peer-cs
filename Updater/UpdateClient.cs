03f464201f046c6a1aa60bb818483c6aa8949e2d
typewriter <takuyaxp@gmail.com>
2023-04-30 22:19:59 +0900

    Separate common classes in updater

--- Updater/UpdateClient.cs
+++ Updater/UpdateClient.cs
@@ -13,7 +13,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace AutoUpdater.Updater
+namespace Updater
 {
     public class UpdateClient
     {
--- Updater/UpdateEntry.cs
+++ Updater/UpdateEntry.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AutoUpdater.Updater
+namespace Updater
 {
     public class UpdateEntry
     {
